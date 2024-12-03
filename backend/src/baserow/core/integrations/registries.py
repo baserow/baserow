adccd502f7834313a9319a2ac683359f92b7ff32
Tsering Paljor <tsering@baserow.io>
2024-12-03 15:53:44 +0400

    Add a new after_import() method to handle integration logic that should run after an import.

--- backend/src/baserow/core/integrations/registries.py
+++ backend/src/baserow/core/integrations/registries.py
@@ -95,6 +95,9 @@ class IntegrationType(
         Hook to trigger any post import logic.
         """
 
+    def after_import(self, user: AbstractUser, instance: Integration):
+        pass
+
     def get_context_data(self, instance: Integration) -> Optional[Dict]:
         """
         Get all the context data for an integration that is required by the editor to
