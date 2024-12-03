c7fe4a37ee4fe3141d433b857302473430999110
Tsering Paljor <tsering@baserow.io>
2024-12-03 11:22:11 +0400

    Lint fix backend

--- backend/src/baserow/core/integrations/handler.py
+++ backend/src/baserow/core/integrations/handler.py
@@ -19,7 +19,6 @@ from baserow.core.storage import ExportZipFile
 from baserow.core.utils import extract_allowed
 
 from .types import IntegrationForUpdate
-from baserow.core.registries import ImportExportConfig
 
 
 class IntegrationHandler:
