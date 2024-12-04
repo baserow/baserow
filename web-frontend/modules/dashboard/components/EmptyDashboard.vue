1ceddac200d71fe1a9f2807523505ef3c238ff79
Petr Stribny <petr@stribny.name>
2024-12-04 15:41:51 +0100

    Add widgets and data sources frontend for dashboards

---
+++ web-frontend/modules/dashboard/components/EmptyDashboard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="empty-dashboard">
+    <div class="empty-dashboard__content">
+      <div class="empty-dashboard__content-title">
+        {{ $t('emptyDashboard.title') }}
+      </div>
+      <div class="empty-dashboard__content-subtitle">
+        {{ $t('emptyDashboard.subtitle') }}
+      </div>
+      <Button
+        type="primary"
+        icon="iconoir-plus"
+        @click="openCreateWidgetModal"
+        >{{ $t('emptyDashboard.addWidget') }}</Button
+      >
+    </div>
+    <CreateWidgetModal
+      ref="createWidgetModal"
+      :dashboard="dashboard"
+      @widget-type-selected="$emit('widget-type-selected', $event)"
+    />
+  </div>
+</template>
+
+<script>
+import CreateWidgetModal from '@baserow/modules/dashboard/components/CreateWidgetModal'
+
+export default {
+  name: 'EmptyDashboard',
+  components: { CreateWidgetModal },
+  props: {
+    dashboard: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    openCreateWidgetModal() {
+      this.$refs.createWidgetModal.show()
+    },
+  },
+}
+</script>
