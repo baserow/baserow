43e2fd418473996ad45c2ac7aa8696ca99da6efc
Tsering Paljor <tsering@baserow.io>
2024-12-04 15:58:45 +0400

    Add tests for execute_integration_post_import_hooks receiver

--- backend/tests/baserow/contrib/integrations/receivers/test_receivers.py
+++ backend/tests/baserow/contrib/integrations/receivers/test_receivers.py
@@ -1,5 +1,4 @@
 from unittest.mock import patch
-
 import pytest
 
 from baserow.core.integrations.receivers import execute_integration_post_import_hooks
@@ -11,7 +10,6 @@ MOCK_LOCAL_BASEROW_PATH = "baserow.contrib.integrations.local_baserow.integratio
 def test_execute_integration_post_import_hooks_returns_early(data_fixture):
     """
     Test the execute_integration_post_import_hooks() receiver.
-
     Ensure that it returns early if the user is None
     """
 
@@ -30,7 +28,6 @@ def test_execute_integration_post_import_hooks_returns_early(data_fixture):
 def test_execute_integration_post_import_hooks_calls_after_import(data_fixture):
     """
     Test the execute_integration_post_import_hooks() receiver.
-
     Ensure that it calls the integration's after_import() hook.
     """
 
