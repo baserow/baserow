6d921e312ee6105ecc2fc0d9855f5d177562e64d
Petr Stribny <petr@stribny.name>
2024-12-04 15:40:52 +0100

    Add data sources and widgets backend for dashboards

--- backend/src/baserow/contrib/dashboard/types.py
+++ backend/src/baserow/contrib/dashboard/types.py
@@ -1,5 +1,20 @@
 from typing import TypedDict
 
+from baserow.core.services.types import ServiceDictSubClass
+
+
+class WidgetDict(TypedDict):
+    id: int
+    order: int
+    type: str
+
+
+class DashboardDataSourceDict(TypedDict):
+    id: int
+    name: str
+    order: int
+    service: ServiceDictSubClass | None
+
 
 class DashboardDict(TypedDict):
     id: int
