bda3c31e88a0ff705d11980aed084cf39d766a59
peter_baserow <peter@baserow.io>
2024-12-17 10:41:41 +0000

    Adding ensure_date and ensure_datetime tests.

---
+++ backend/tests/baserow/core/formula/test_validator.py
@@ -0,0 +1,22 @@
+from datetime import date, datetime
+
+import pytest
+from django.core.exceptions import ValidationError
+
+from baserow.core.formula.validator import ensure_date, ensure_datetime
+
+
+def test_ensure_date():
+    assert ensure_date(None) is None
+    assert ensure_date("2024-12-17") == date(2024, 12, 17)
+    with pytest.raises(ValidationError) as exc:
+        ensure_date("invalid")
+    assert exc.value.args[0] == "Value cannot be converted to a date."
+
+
+def test_ensure_datetime():
+    assert ensure_datetime(None) is None
+    assert ensure_datetime("2024-12-17 12:00") == datetime(2024, 12, 17, 12, 0, 0)
+    with pytest.raises(ValidationError) as exc:
+        ensure_datetime("invalid")
+    assert exc.value.args[0] == "Value cannot be converted to a datetime."
