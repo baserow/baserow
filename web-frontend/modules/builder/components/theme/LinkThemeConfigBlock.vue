27ecc176fbaefd11e1b0905e51bb253e0c0c3e15
Tsering Paljor <tsering@baserow.io>
2024-12-24 14:52:29 +0400

    Add ability to set link font weight in frontend

--- web-frontend/modules/builder/components/theme/LinkThemeConfigBlock.vue
+++ web-frontend/modules/builder/components/theme/LinkThemeConfigBlock.vue
@@ -54,6 +54,20 @@
           </template>
         </FormGroup>
         <FormGroup
+          horizontal-narrow
+          small-label
+          class="margin-bottom-2"
+          :label="$t('linkThemeConfigBlock.weight')"
+        >
+          <FontWeightSelector v-model="values.link_font_weight" />
+          <template #after-input>
+            <ResetButton
+              v-model="values.link_font_weight"
+              :default-value="theme?.link_font_weight"
+            />
+          </template>
+        </FormGroup>
+        <FormGroup
           v-if="!extraArgs?.noAlignment"
           horizontal-narrow
           small-label
