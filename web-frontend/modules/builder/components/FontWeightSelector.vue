bc0b38a15c37486a1502135d1e12e65d3c99395d
Tsering Paljor <tsering@baserow.io>
2024-12-24 11:42:57 +0400

    Add FontWeightSelector component

---
+++ web-frontend/modules/builder/components/FontWeightSelector.vue
@@ -0,0 +1,30 @@
+<template>
+  <Dropdown :value="value" fixed-items @input="$emit('input', $event)">
+    <DropdownItem
+      v-for="fontWeight in fontWeights"
+      :key="fontWeight.getType()"
+      :value="fontWeight.getType()"
+      :name="fontWeight.name"
+    />
+  </Dropdown>
+</template>
+
+<script>
+export default {
+  name: 'FontWeightSelector',
+  props: {
+    value: {
+      type: String,
+      required: false,
+      default: 'Regular',
+    },
+  },
+  computed: {
+    fontWeights() {
+      return Object.values(this.$registry.getAll('fontWeight')).sort((a, b) =>
+        a.weight < b.weight
+      )
+    },
+  },
+}
+</script>
