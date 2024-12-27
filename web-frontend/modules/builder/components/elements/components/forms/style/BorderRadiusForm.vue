76c0e24722204230568a031097768842175cd2b2
Tsering Paljor <tsering@baserow.io>
2024-12-27 17:43:32 +0400

    Add docstrings and comments

--- web-frontend/modules/builder/components/elements/components/forms/style/BorderRadiusForm.vue
+++ web-frontend/modules/builder/components/elements/components/forms/style/BorderRadiusForm.vue
@@ -27,6 +27,8 @@ import { required, integer, between } from 'vuelidate/lib/validators'
 import form from '@baserow/modules/core/mixins/form'
 import PixelValueSelector from '@baserow/modules/builder/components/PixelValueSelector'
 
+const MAX_BORDER_RADIUS_PX = 100
+
 export default {
   name: 'BorderRadiusForm',
   components: { PixelValueSelector },
@@ -73,6 +75,12 @@ export default {
     }
   },
   computed: {
+    /**
+     * Returns an iterable of borderRadiuses that are allowed.
+     *
+     * The alternative is to use a `v-for` and `v-if` in the `FormGroup`, which
+     * isn't a recommended practice.
+     */
     allowedBorderRadiuses() {
       return this.borderRadiuses.filter((item) => {
         const computedMethodName = `${_.camelCase(item.name)}IsAllowed`
@@ -81,28 +89,40 @@ export default {
     },
     borderRadiusTopLeftError() {
       if (this.$v.values.top_left.$invalid) {
-        return this.$t('error.minMaxValueField', { min: 0, max: 100 })
+        return this.$t('error.minMaxValueField', {
+          min: 0,
+          max: MAX_BORDER_RADIUS_PX,
+        })
       } else {
         return ''
       }
     },
     borderRadiusTopRightError() {
       if (this.$v.values.top_right.$invalid) {
-        return this.$t('error.minMaxValueField', { min: 0, max: 100 })
+        return this.$t('error.minMaxValueField', {
+          min: 0,
+          max: MAX_BORDER_RADIUS_PX,
+        })
       } else {
         return ''
       }
     },
     borderRadiusBottomLeftError() {
       if (this.$v.values.bottom_left.$invalid) {
-        return this.$t('error.minMaxValueField', { min: 0, max: 100 })
+        return this.$t('error.minMaxValueField', {
+          min: 0,
+          max: MAX_BORDER_RADIUS_PX,
+        })
       } else {
         return ''
       }
     },
     borderRadiusBottomRightError() {
       if (this.$v.values.bottom_right.$invalid) {
-        return this.$t('error.minMaxValueField', { min: 0, max: 100 })
+        return this.$t('error.minMaxValueField', {
+          min: 0,
+          max: MAX_BORDER_RADIUS_PX,
+        })
       } else {
         return ''
       }
@@ -115,11 +135,20 @@ export default {
     emitChange(newValues) {
       this.$emit('input', newValues)
     },
+    /**
+     * Checks if the input is in error.
+     *
+     * Given a name, this method dynamically finds and returns the correct
+     * computed method's value.
+     */
     getBorderRadiusError(name) {
       // Convert name to title-case, e.g. "Top right" to "Top Right"
       const title = _.startCase(name).replace(/ /g, '')
       return this[`borderRadius${title}Error`]
     },
+    /**
+     * A helper to dynamically access the correct prop value.
+     */
     styleIsAllowed(name) {
       const camelCase = _.camelCase(name)
       return this[`${camelCase}IsAllowed`]
@@ -131,22 +160,22 @@ export default {
         top_left: {
           required,
           integer,
-          between: between(0, 100),
+          between: between(0, MAX_BORDER_RADIUS_PX),
         },
         top_right: {
           required,
           integer,
-          between: between(0, 100),
+          between: between(0, MAX_BORDER_RADIUS_PX),
         },
         bottom_left: {
           required,
           integer,
-          between: between(0, 100),
+          between: between(0, MAX_BORDER_RADIUS_PX),
         },
         bottom_right: {
           required,
           integer,
-          between: between(0, 100),
+          between: between(0, MAX_BORDER_RADIUS_PX),
         },
       },
     }
