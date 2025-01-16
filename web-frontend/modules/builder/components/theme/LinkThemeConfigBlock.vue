122027a18489c055892241216d0d0300fd19e793
Jeremie Pardou <jeremie@baserow.io>
2025-01-16 11:47:23 +0100

    Add missing font property

--- web-frontend/modules/builder/components/theme/LinkThemeConfigBlock.vue
+++ web-frontend/modules/builder/components/theme/LinkThemeConfigBlock.vue
@@ -59,7 +59,10 @@
           class="margin-bottom-2"
           :label="$t('linkThemeConfigBlock.weight')"
         >
-          <FontWeightSelector v-model="values.link_font_weight" />
+          <FontWeightSelector
+            v-model="values.link_font_weight"
+            :font="values.link_font_family"
+          />
           <template #after-input>
             <ResetButton
               v-model="values.link_font_weight"
