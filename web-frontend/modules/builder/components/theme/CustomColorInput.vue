591173bf5a40398b1f55ecf31967b95d02136593
Tsering Paljor <tsering@baserow.io>
2025-01-23 12:35:16 +0400

    Add validation to custom color name

--- web-frontend/modules/builder/components/theme/CustomColorInput.vue
+++ web-frontend/modules/builder/components/theme/CustomColorInput.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="custom-color-input__container">
-    <FormInput
-      :value="value.name"
-      class="custom-color-input__input-name margin-bottom-2"
-      @input="(newValue) => updateCustomColorName(newValue)"
-    />
-
-    <ColorInput
-      :value="value.color"
-      small
-      @input="(newValue) => updateExistingColor(newValue)"
-    />
+  <div class="custom-color-input__container margin-bottom-2">
+    <FormGroup required class="custom-color-input__form-group" :error-message="colorNameError">
+      <FormInput
+        :value="value.name"
+        class="custom-color-input__input-name"
+        @input="(newValue) => updateCustomColorName(newValue)"
+      />
+    </FormGroup>
     
+    <FormGroup required>
+      <ColorInput
+        :value="value.color"
+        small
+        @input="(newValue) => updateExistingColor(newValue)"
+      />
+    </FormGroup>  
     <ButtonIcon icon="iconoir-bin" @click="$emit('deleteCustomColor')" />
   </div>
 </template>
 
 <script>
+const COLOR_NAME_MAX_LENGTH = 255
+
 export default {
   props: {
     value: {
@@ -28,19 +33,34 @@ export default {
     return {
       nameTimeout: null,
       colorTimeout: null,
+      colorNameError: '',
     }
   },
   methods: {
     updateCustomColorName(newValue) {
+      this.colorNameError = ''
+      
+      const name = newValue.trim()
+      if (!name) {
+        this.colorNameError = this.$t('error.requiredField')
+      } else if (name.length > COLOR_NAME_MAX_LENGTH) {
+        this.colorNameError = this.$t('error.maxLength', { max: COLOR_NAME_MAX_LENGTH })
+      }
+
+      if (this.colorNameError) {
+        return
+      }
+
       clearTimeout(this.nameTimeout)
       this.nameTimeout = setTimeout(() => {
-        this.$emit('input', {name: newValue, color: this.value.color})
+        this.$emit('input', {name, color: this.value.color, value: this.value.value})
       }, 500)      
     },
+
     updateExistingColor(newValue) {
       clearTimeout(this.colorTimeout)
       this.colorTimeout = setTimeout(() => {
-        this.$emit('input', {name: this.value.name, color: newValue})
+        this.$emit('input', {name: this.value.name, color: newValue, value: this.value.value})
       }, 500)      
     },
   },
