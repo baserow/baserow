7bbf2167cf6a4cca95fc53e13fe0dd0a20f6d784
Davide Silvestri <davide@baserow.io>
2025-01-29 17:22:00 +0100

    Refactor tests

--- backend/tests/baserow/contrib/database/field/test_field_single_select_options.py
+++ backend/tests/baserow/contrib/database/field/test_field_single_select_options.py
@@ -1,13 +1,19 @@
+from unittest.mock import patch
+
 from django.db import connection
+from django.test import override_settings
 
 import pytest
 from psycopg2 import sql
 
+from baserow.contrib.database.fields.handler import FieldHandler
+from baserow.contrib.database.fields.registries import field_type_registry
 from baserow.contrib.database.rows.handler import RowHandler
 
 
-# @pytest.mark.disabled_in_ci
+# @pytest.mark.disabled_in_ci  # Disable this test in CI in next release.
 @pytest.mark.django_db
+@override_settings(BASEROW_DISABLE_MODEL_CACHE=True)
 def test_migration_rows_with_deleted_singleselect_options(
     data_fixture, migrator, teardown_table_metadata
 ):
@@ -19,101 +25,102 @@ def test_migration_rows_with_deleted_singleselect_options(
     migrator.migrate(migrate_from)
 
     user = data_fixture.create_user()
-    workspace = data_fixture.create_workspace(user=user)
-    database = data_fixture.create_database_application(workspace=workspace, user=user)
-
-    # Test that it doesn't crash on larger dataset
-    row_handler = RowHandler()
-
-    table_for_values = data_fixture.create_database_table(
-        database=database, name=f"Table for values"
-    )
-    text_field_for_values = data_fixture.create_text_field(
-        table=table_for_values, name="text_field", order=0
-    )
-    option_field_for_values = data_fixture.create_single_select_field(
-        table=table_for_values, name="option_field", order=1
-    )
+    database = data_fixture.create_database_application(user=user)
+    table = data_fixture.create_database_table(database=database)
+    single_select_field = data_fixture.create_single_select_field(table=table)
 
     option_a = data_fixture.create_select_option(
-        field=option_field_for_values, value=f"Option A"
+        field=single_select_field, value=f"Option A"
     )
     option_b = data_fixture.create_select_option(
-        field=option_field_for_values, value=f"Option B"
+        field=single_select_field, value=f"Option B"
     )
-    option_c = data_fixture.create_select_option(
-        field=option_field_for_values, value=f"Option C"
+
+    _, row_with_b = RowHandler().force_create_rows(
+        user=user,
+        table=table,
+        rows_values=[
+            {single_select_field.db_column: opt.id} for opt in (option_a, option_b)
+        ],
     )
-    rows = [
-        {
-            text_field_for_values.db_column: f"Row #1",
-            option_field_for_values.db_column: option_a.id,
-        },
-        {
-            text_field_for_values.db_column: f"Row #2",
-            option_field_for_values.db_column: option_b.id,
-        },
-        {
-            text_field_for_values.db_column: f"Row #3",
-            option_field_for_values.db_column: option_c.id,
-        },
-        {
-            text_field_for_values.db_column: f"Row #4",
-            option_field_for_values.db_column: option_b.id,
-        },
-    ]
 
-    row_handler.create_rows(user=user, table=table_for_values, rows_values=rows)
+    single_select_field_type = field_type_registry.get_by_model(single_select_field)
 
-    option_b.delete()
-
-    # Update Row #4 to contain option value that does not exist
-    with connection.cursor() as cursor:
-        result = cursor.execute(
-            sql.SQL(
-                "UPDATE {table} SET {field} = 8 WHERE {text_field} = 'Row #4'"
-            ).format(
-                table=sql.Identifier(
-                    f"database_table_{option_field_for_values.table.id}"
-                ),
-                field=sql.Identifier(f"field_{option_field_for_values.id}"),
-                text_field=sql.Identifier(f"field_{text_field_for_values.id}"),
-            )
+    # Temporarily mock, as before the MR that introduced the migration
+    with patch.object(
+        single_select_field_type, "before_field_options_update", return_value=None
+    ):
+        FieldHandler().update_field(
+            user,
+            single_select_field,
+            select_options=[
+                {
+                    "id": option_a.id,
+                    "value": "A",
+                    "color": "blue",
+                }
+            ],
         )
 
-    # Row is properly updated
-    with connection.cursor() as cursor:
-        cursor.execute(
-            sql.SQL(
-                "SELECT {text_field}, {option_field} FROM {table} WHERE {text_field} = 'Row #4'"
-            ).format(
-                table=sql.Identifier(
-                    f"database_table_{option_field_for_values.table.id}"
+    def check_row_b_option_id():
+        with connection.cursor() as cursor:
+            cursor.execute(
+                sql.SQL("SELECT {option_field} FROM {table} WHERE id = %s").format(
+                    table=sql.Identifier(f"database_table_{table.id}"),
+                    option_field=sql.Identifier(single_select_field.db_column),
                 ),
-                text_field=sql.Identifier(f"field_{text_field_for_values.id}"),
-                option_field=sql.Identifier(f"field_{option_field_for_values.id}"),
+                [row_with_b.id],
             )
-        )
-        rows_from_db = cursor.fetchall()
-    assert len(rows_from_db) == 1
-    assert rows_from_db[0] == ("Row #4", 8)
+            return cursor.fetchall()
 
+    assert check_row_b_option_id() == [(option_b.id,)]
+
+    # the migration should remove all the deleted options ids
     migrator.migrate(migrate_to)
 
-    # After migration, the row is updated to contain None
-    with connection.cursor() as cursor:
-        cursor.execute(
-            sql.SQL(
-                "SELECT {text_field}, {option_field} FROM {table} WHERE {text_field} = 'Row #4'"
-            ).format(
-                table=sql.Identifier(
-                    f"database_table_{option_field_for_values.table.id}"
-                ),
-                text_field=sql.Identifier(f"field_{text_field_for_values.id}"),
-                option_field=sql.Identifier(f"field_{option_field_for_values.id}"),
-            )
-        )
-        rows_from_db = cursor.fetchall()
+    assert check_row_b_option_id() == [(None,)]
 
-    assert len(rows_from_db) == 1
-    assert rows_from_db[0] == ("Row #4", None)
+
+@pytest.mark.django_db
+def test_single_select_ids_are_removed_from_rows_when_deleted(data_fixture):
+    user = data_fixture.create_user()
+    database = data_fixture.create_database_application(user=user)
+    table = data_fixture.create_database_table(database=database)
+    single_select_field = data_fixture.create_single_select_field(table=table)
+
+    option_a = data_fixture.create_select_option(field=single_select_field, value=f"A")
+    option_b = data_fixture.create_select_option(field=single_select_field, value=f"B")
+
+    _, row_with_b = RowHandler().force_create_rows(
+        user=user,
+        table=table,
+        rows_values=[
+            {single_select_field.db_column: opt.id} for opt in (option_a, option_b)
+        ],
+    )
+
+    # Keep only A, and remove B
+    FieldHandler().update_field(
+        user,
+        single_select_field,
+        select_options=[
+            {
+                "id": option_a.id,
+                "value": "A",
+                "color": "blue",
+            }
+        ],
+    )
+
+    def check_row_b_option_id():
+        with connection.cursor() as cursor:
+            cursor.execute(
+                sql.SQL("SELECT {option_field} FROM {table} WHERE id = %s").format(
+                    table=sql.Identifier(f"database_table_{table.id}"),
+                    option_field=sql.Identifier(single_select_field.db_column),
+                ),
+                [row_with_b.id],
+            )
+            return cursor.fetchall()
+
+    assert check_row_b_option_id() == [(None,)]
