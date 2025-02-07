23f6e095ef22442efe3abe421c305ca3becfe0c9
Tsering Paljor <tsering@baserow.io>
2025-02-07 11:47:17 +0400

    Add initial MenuElement component

---
+++ web-frontend/modules/builder/components/elements/components/MenuElement.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="menu-element" :style="getStyleOverride(element.variant)">
+    Menu Element!
+  </div>
+</template>
+
+<script>
+import element from '@baserow/modules/builder/mixins/element'
+import resolveElementUrl from '@baserow/modules/builder/utils/urlResolution'
+import { ensureString } from '@baserow/modules/core/utils/validator'
+
+/**
+ * @typedef MenuElement
+ */
+
+export default {
+  name: 'MenuElement',
+  mixins: [element],
+  props: {
+    /**
+     * @type {MenuElement}
+     */
+    element: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {},
+}
+</script>
