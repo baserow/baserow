9feb802f4c960da13b54f6c429d6f3c34f38c413
Jeremie Pardou <jeremie@baserow.io>
2025-02-18 17:05:02 +0100

    Add is none to only_record_id condition

--- backend/src/baserow/contrib/integrations/local_baserow/service_types.py
+++ backend/src/baserow/contrib/integrations/local_baserow/service_types.py
@@ -1082,7 +1082,7 @@ class LocalBaserowListRowsUserServiceType(
             # Ensure that only used fields are fetched from the database.
             queryset = queryset.only(*available_fields.intersection(only_field_names))
 
-        if dispatch_context.only_record_id:
+        if dispatch_context.only_record_id is not None:
             queryset = queryset.filter(id=dispatch_context.only_record_id)
 
         offset, count = dispatch_context.range(service)
