60258065741533ae23f41e724f1fa9e7befceebd
Petr Stribny <petr@stribny.name>
2025-02-24 04:11:35 +0100

    Only allow compatible aggregation types for charts

---
+++ enterprise/backend/src/baserow_enterprise/integrations/registries.py
@@ -0,0 +1,21 @@
+from baserow.contrib.database.views.exceptions import (
+    AggregationTypeAlreadyRegistered,
+    AggregationTypeDoesNotExist,
+)
+from baserow.core.registry import Registry
+
+
+class GroupedAggregationTypeRegistry(Registry):
+    """
+    The main registry for storing aggregation types compatible
+    with the grouped aggregate service.
+    """
+
+    name = "grouped_aggregations"
+    does_not_exist_exception_class = AggregationTypeDoesNotExist
+    already_registered_exception_class = AggregationTypeAlreadyRegistered
+
+
+grouped_aggregation_registry: GroupedAggregationTypeRegistry = (
+    GroupedAggregationTypeRegistry()
+)
