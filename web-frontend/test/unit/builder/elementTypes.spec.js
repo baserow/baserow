12092daf2bc297d719fbb89c960edbd7922848bb
Tsering Paljor <tsering@baserow.io>
2025-02-26 17:41:13 +0400

    Lint fix frontend

--- web-frontend/test/unit/builder/elementTypes.spec.js
+++ web-frontend/test/unit/builder/elementTypes.spec.js
@@ -919,63 +919,49 @@ describe('elementTypes tests', () => {
       }
 
       // Menu element with zero Menu items is invalid.
-      expect(elementType.isInError({ page: {}, element, builder })).toBe(
-        true
-      )
+      expect(elementType.isInError({ page: {}, element, builder })).toBe(true)
 
-      let menuItem = {
+      const menuItem = {
         type: 'button',
         name: 'foo button',
       }
       element.menu_items = [menuItem]
 
       // Button Menu item without workflow actions is invalid.
-      expect(elementType.isInError({ page, element, builder })).toBe(
-        true
-      )
+      expect(elementType.isInError({ page, element, builder })).toBe(true)
 
       page.workflowActions = [{ element_id: 50, type: 'open_page' }]
-      element.menu_items[0].name = ""
+      element.menu_items[0].name = ''
 
       // Button Menu item with empty name is invalid.
-      expect(elementType.isInError({ page, element, builder })).toBe(
-        true
-      )
+      expect(elementType.isInError({ page, element, builder })).toBe(true)
 
       element.menu_items[0].type = 'link'
       element.menu_items[0].name = ''
 
       // Link Menu item with empty name is invalid.
-      expect(elementType.isInError({ page, element, builder })).toBe(
-        true
-      )
+      expect(elementType.isInError({ page, element, builder })).toBe(true)
 
       element.menu_items[0].name = 'sub link'
       element.menu_items[0].navigation_type = 'page'
       element.menu_items[0].navigate_to_page_id = ''
 
       // Link Menu item - sublink with Page navigation but no page ID is invalid.
-      expect(elementType.isInError({ page, element, builder })).toBe(
-        true
-      )
+      expect(elementType.isInError({ page, element, builder })).toBe(true)
 
       element.menu_items[0].name = 'sub link'
       element.menu_items[0].navigation_type = 'custom'
       element.menu_items[0].navigate_to_url = ''
 
       // Link Menu item - sublink with custom navigation but no URL is invalid.
-      expect(elementType.isInError({ page, element, builder })).toBe(
-        true
-      )
+      expect(elementType.isInError({ page, element, builder })).toBe(true)
 
       // Valid Button Menu item
       element.menu_items[0].type = 'button'
       element.menu_items[0].name = 'foo button'
       page.workflowActions = [{ element_id: 50, type: 'open_page' }]
 
-      expect(elementType.isInError({ page, element, builder} )).toBe(
-        false
-      )
+      expect(elementType.isInError({ page, element, builder })).toBe(false)
 
       // Valid Link Menu item - page
       element.menu_items[0].type = 'link'
@@ -983,9 +969,7 @@ describe('elementTypes tests', () => {
       element.menu_items[0].navigation_type = 'page'
       element.menu_items[0].navigate_to_page_id = 10
 
-      expect(elementType.isInError({ page, element, builder} )).toBe(
-        false
-      )
+      expect(elementType.isInError({ page, element, builder })).toBe(false)
 
       // Valid Link Menu item - custom
       element.menu_items[0].type = 'link'
@@ -993,9 +977,7 @@ describe('elementTypes tests', () => {
       element.menu_items[0].navigation_type = 'custom'
       element.menu_items[0].navigate_to_url = 'https://www.baserow.io'
 
-      expect(elementType.isInError({ page, element, builder} )).toBe(
-        false
-      )
+      expect(elementType.isInError({ page, element, builder })).toBe(false)
     })
   })
 
