73534c449131cbf1459990806a784eb24c138efa
Jeremie Pardou <jeremie@baserow.io>
2025-04-01 08:59:45 +0200

    Update tests

--- backend/tests/baserow/contrib/builder/elements/test_menu_element_type.py
+++ backend/tests/baserow/contrib/builder/elements/test_menu_element_type.py
@@ -416,41 +416,39 @@ def test_import_export(menu_element_fixture, data_fixture):
 
     assert MenuElement.objects.count() == 0
     assert MenuItemElement.objects.count() == 0
-    assert NotificationWorkflowAction.objects.count() == 0
 
     # After importing the Menu element the menu items should be correctly
     # imported as well.
-    id_mapping = defaultdict(lambda: MirrorDict())
+    id_mapping = defaultdict(MirrorDict)
     menu_element_type.import_serialized(page, exported, id_mapping)
 
     menu_element = MenuElement.objects.first()
 
     # Ensure the Menu Items have been imported correctly
-    button_item = menu_element.menu_items.get(uid=uid_1)
-    assert button_item.name == "Greet"
+    # and uids should have been updated
+    button_item = menu_element.menu_items.get(name="Greet")
+    assert button_item.type == MenuItemElement.TYPES.BUTTON
+    assert button_item.uid != uid_1
 
-    link_item = menu_element.menu_items.get(uid=uid_2)
-    assert link_item.name == "Link A"
+    link_item = menu_element.menu_items.get(name="Link A")
+    assert link_item.type == MenuItemElement.TYPES.LINK
+    assert link_item.uid != uid_2
 
-    sublinks_item = menu_element.menu_items.get(uid=uid_3)
-    assert sublinks_item.name == "Sublinks"
+    sublinks_item = menu_element.menu_items.get(name="Sublinks")
+    assert sublinks_item.uid != uid_3
 
-    sublink_a = menu_element.menu_items.get(uid=uid_4)
-    assert sublink_a.name == "Sublink A"
+    sublink_a = menu_element.menu_items.get(name="Sublink A")
+    assert sublink_a.uid != uid_4
 
 
 @pytest.mark.django_db
 def test_delete_duplicated_menu_doesnt_affect_initial_element(
     menu_element_fixture, data_fixture
 ):
-    page = menu_element_fixture["page_a"]
     menu_element = menu_element_fixture["menu_element"]
 
     # Create a Menu Element with Menu items.
     uid_1 = uuid.uuid4()
-    uid_2 = uuid.uuid4()
-    uid_3 = uuid.uuid4()
-    uid_4 = uuid.uuid4()
     menu_item_1 = {
         "name": "Greet",
         "type": MenuItemElement.TYPES.BUTTON,
@@ -458,44 +456,28 @@ def test_delete_duplicated_menu_doesnt_affect_initial_element(
         "uid": uid_1,
         "children": [],
     }
-    menu_item_2 = {
-        "name": "Link A",
-        "type": MenuItemElement.TYPES.BUTTON,
-        "menu_item_order": 1,
-        "uid": uid_2,
-        "children": [],
-    }
-    menu_item_3 = {
-        "name": "Sublinks",
-        "type": MenuItemElement.TYPES.LINK,
-        "menu_item_order": 2,
-        "uid": uid_3,
-        "children": [
-            {
-                "name": "Sublink A",
-                "type": MenuItemElement.TYPES.LINK,
-                "menu_item_order": 3,
-                "uid": uid_4,
-                "navigate_to_page_id": page.id,
-            }
-        ],
-    }
 
-    data = {"menu_items": [menu_item_1, menu_item_2, menu_item_3]}
+    data = {"menu_items": [menu_item_1]}
     ElementHandler().update_element(menu_element, **data)
 
-    for uid in [uid_1, uid_2]:
-        data_fixture.create_workflow_action(
-            NotificationWorkflowAction,
-            page=menu_element.page,
-            element=menu_element,
-            event=f"{uid}_click",
-        )
+    workflow_action = data_fixture.create_workflow_action(
+        NotificationWorkflowAction,
+        page=menu_element.page,
+        element=menu_element,
+        event=f"{uid_1}_click",
+    )
 
-    duplicated_element = ElementHandler().duplicate_element(menu_element)["elements"][0]
+    duplicated = ElementHandler().duplicate_element(menu_element)
 
-    assert NotificationWorkflowAction.objects.count() == 4
+    element = duplicated["elements"][0]
+    duplicated_workflow_action = duplicated["workflow_actions"][0]
 
-    ElementHandler().delete_element(duplicated_element)
+    duplicated_workflow_action.event != workflow_action.event
 
     assert NotificationWorkflowAction.objects.count() == 2
+
+    ElementHandler().delete_element(element)
+
+    # We want to make sure that deleting the clone doesn't delete the initial event
+    assert NotificationWorkflowAction.objects.count() == 1
+    assert NotificationWorkflowAction.objects.first().event == workflow_action.event
