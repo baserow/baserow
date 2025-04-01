2766f6cf2672f9aad3b33695460b10e27195d979
Jeremie Pardou <jeremie@baserow.io>
2025-04-01 15:59:25 +0200

    Remove logs

--- e2e-tests/tests/builder/builderTemplate.spec.ts
+++ e2e-tests/tests/builder/builderTemplate.spec.ts
@@ -4,11 +4,8 @@ import { expect, test } from "../baserowTest";
 test.describe("Builder template application test suite", () => {
   test("Can show an AB template", async ({ page }) => {
     const templatePage = new TemplatePage(page, "ab_ivory_theme");
-    console.log("yay");
 
     await templatePage.goto();
-
-    console.log("pof");
 
     await expect(
       page.locator(".ab-heading--h3").getByText("Rebranding website")
