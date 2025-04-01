f509459de3e534999b2831ed8cd0b80718a60404
Jeremie Pardou <jeremie@baserow.io>
2025-04-01 15:12:47 +0200

    Fix template AB permissions

--- backend/src/baserow/api/user_sources/views.py
+++ backend/src/baserow/api/user_sources/views.py
@@ -83,6 +83,12 @@ from baserow.core.user_sources.service import UserSourceService
 class UserSourcesView(APIView):
     permission_classes = (IsAuthenticated,)
 
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+
+        return super().get_permissions()
+
     @extend_schema(
         parameters=[
             OpenApiParameter(
