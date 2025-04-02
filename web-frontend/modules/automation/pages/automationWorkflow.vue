e03311882015ecd62e4b7610e140415dd5ed366b
Tsering Paljor <tsering@baserow.io>
2025-04-02 15:38:47 +0400

    Add initial page for automation workflow

---
+++ web-frontend/modules/automation/pages/automationWorkflow.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="automation-app">
+    <AutomationHeader :automation="automation" />
+    <div class="layout__col-2-2 automation-app__content">
+      <div class="automation-app__content-header">
+        <div class="automation-app__title">{{ currentWorkflow.name }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AutomationHeader from '@baserow/modules/automation/components/AutomationHeader'
+
+export default {
+  name: 'AutomationWorkflow',
+  components: { AutomationHeader },
+  provide() {
+    return {
+      workspace: this.workspace,
+      automation: this.automation,
+      currentWorkflow: this.currentWorkflow,
+    }
+  },
+  layout: 'app',
+  async asyncData({ store, params, error, $registry }) {
+    const automationId = parseInt(params.automationId)
+    const workflowId = parseInt(params.workflowId)
+
+    const data = {}
+    try {
+      const automation = await store.dispatch(
+        'application/selectById',
+        automationId
+      )
+      const workspace = await store.dispatch(
+        'workspace/selectById',
+        automation.workspace.id
+      )
+
+      const workflow = store.getters['automationWorkflow/getById'](automation, workflowId)
+
+      await store.dispatch('automationWorkflow/selectById', {
+        automation,
+        workflowId,
+      })
+
+      data.workspace = workspace
+      data.automation = automation
+      data.currentWorkflow = workflow
+
+    } catch (e) {
+      // TODO: replace with translation
+      return error({ statusCode: 404, message: 'Automation not found.' })
+    }
+    return data
+  },
+}
+</script>
