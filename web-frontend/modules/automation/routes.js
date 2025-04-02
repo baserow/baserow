4af3aaa50395a180424e0d591cfc8396f30e9f6b
Tsering Paljor <tsering@baserow.io>
2025-04-02 15:39:52 +0400

    Add route for automation workflow page

--- web-frontend/modules/automation/routes.js
+++ web-frontend/modules/automation/routes.js
@@ -11,4 +11,15 @@ export const routes = [
       return p
     },
   },
+  {
+    name: 'automation-workflow',
+    path: '/automation/:automationId/workflow/:workflowId',
+    component: path.resolve(__dirname, 'pages/automationWorkflow.vue'),
+    props(route) {
+      const p = { ...route.params }
+      p.automationId = parseInt(p.automationId)
+      p.workflowId = parseInt(p.workflowId)
+      return p
+    },
+  },
 ]
