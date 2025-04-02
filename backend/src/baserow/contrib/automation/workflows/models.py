e0c428d829052c3598d50d6d0d33ecca71d492ed
Tsering Paljor <tsering@baserow.io>
2025-04-02 15:30:15 +0400

    Add AutomationWorkflow model

---
+++ backend/src/baserow/contrib/automation/workflows/models.py
@@ -0,0 +1,36 @@
+import typing
+
+from django.db import models
+
+from baserow.core.mixins import (
+    CreatedAndUpdatedOnMixin,
+    HierarchicalModelMixin,
+    OrderableMixin,
+    TrashableModelMixin,
+)
+from baserow.contrib.automation.constants import WORKFLOW_NAME_MAX_LEN
+
+if typing.TYPE_CHECKING:
+    from baserow.contrib.automation.models import Automation
+
+
+class AutomationWorkflow(
+    HierarchicalModelMixin,
+    TrashableModelMixin,
+    CreatedAndUpdatedOnMixin,
+    OrderableMixin,
+    ):
+    
+    automation = models.ForeignKey("automation.Automation", on_delete=models.CASCADE, related_name="workflows")
+
+    name = models.CharField(max_length=WORKFLOW_NAME_MAX_LEN)
+
+    order = models.PositiveIntegerField()
+
+    def get_parent(self):
+        return self.automation
+
+    @classmethod
+    def get_last_order(cls, automation: "Automation"):
+        queryset = AutomationWorkflow.objects.filter(automation=automation)
+        return cls.get_highest_order_of_queryset(queryset) + 1
