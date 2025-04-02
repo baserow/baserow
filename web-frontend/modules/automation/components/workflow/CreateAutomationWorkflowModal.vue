6c0b845303d74401e782dc1ca4ade13950e4cdaa
Tsering Paljor <tsering@baserow.io>
2025-04-02 15:37:48 +0400

    Add form + modal for creating a new workflow

---
+++ web-frontend/modules/automation/components/workflow/CreateAutomationWorkflowModal.vue
@@ -0,0 +1,78 @@
+<template>
+  <Modal>
+    <h2 class="box__title">
+      {{ $t('createAutomationWorkflowModal.header') }}
+    </h2>
+    <AutomationWorkflowSettingsForm
+      ref="automationWorkflowForm"
+      :automation="automation"
+      is-creation
+      @submitted="addWorkflow"
+    >
+      <div class="actions actions--right">
+        <Button size="large" :loading="loading">
+          {{ $t('createAutomationWorkflowModal.submit') }}
+        </Button>
+      </div>
+    </AutomationWorkflowSettingsForm>
+  </Modal>
+</template>
+
+<script>
+import modal from '@baserow/modules/core/mixins/modal'
+import { notifyIf } from '@baserow/modules/core/utils/error'
+import AutomationWorkflowSettingsForm from '@baserow/modules/automation/components/workflow/settings/AutomationWorkflowSettingsForm'
+
+export default {
+  name: 'CreateAutomationWorkflowModal',
+  components: { AutomationWorkflowSettingsForm },
+  mixins: [modal],
+  provide() {
+    return {
+      currentPage: null,
+      automation: this.automation,
+      workspace: this.workspace,
+    }
+  },
+  props: {
+    workspace: {
+      type: Object,
+      required: true,
+    },
+    automation: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      loading: false,
+    }
+  },
+  methods: {
+    async addWorkflow({name}) {
+      this.loading = true
+      try {
+        const workflow = await this.$store.dispatch('automationWorkflow/create', {
+          automation: this.automation,
+          name,
+        })
+        console.log('created workflow: ', workflow)
+        this.$refs.automationWorkflowForm.v$.$reset()
+        this.hide()
+        // this.$router.push(
+        //   {
+        //     name: 'builder-page',
+        //     params: { builderId: this.builder.id, pageId: page.id },
+        //   },
+        //   null,
+        //   () => {}
+        // )
+      } catch (error) {
+        notifyIf(error, 'application')
+      }
+      this.loading = false
+    },
+  },
+}
+</script>
