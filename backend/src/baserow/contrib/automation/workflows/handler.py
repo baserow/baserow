a14a1075b6ab769830c795e939e2c944287f31c2
Tsering Paljor <tsering@baserow.io>
2025-04-02 15:31:02 +0400

    Add handler and service for workflows

---
+++ backend/src/baserow/contrib/automation/workflows/handler.py
@@ -0,0 +1,297 @@
+from collections import defaultdict
+from typing import Any, Dict, List, Optional
+from zipfile import ZipFile
+
+from django.core.files.storage import Storage
+from django.db.models import QuerySet
+
+from baserow.core.exceptions import IdDoesNotExist
+from baserow.core.utils import ChildProgressBuilder, MirrorDict, find_unused_name
+from baserow.core.exceptions import IdDoesNotExist
+from baserow.contrib.automation.models import Automation
+from baserow.contrib.automation.workflows.models import AutomationWorkflow
+from baserow.contrib.automation.constants import IMPORT_SERIALIZED_IMPORTING, WORKFLOW_NAME_MAX_LEN
+from baserow.contrib.automation.workflows.exceptions import (
+    AutomationWorkflowDoesNotExist,
+    AutomationWorkflowNotInAutomation,
+)
+from baserow.contrib.automation.types import AutomationWorkflowDict
+
+
+class AutomationWorkflowHandler:
+    def get_workflow(self, workflow_id: int, base_queryset: Optional[QuerySet] = None) -> AutomationWorkflow:
+        """
+        Gets a AutomationWorkflow by its ID.
+
+        :param workflow_id: The ID of the AutomationWorkflow.
+        :param base_queryset: Can be provided to already filter or apply performance
+            improvements to the queryset when it's being executed.
+        :raises AutomationWorkflowDoesNotExist: If the workflow doesn't exist.
+        :return: The model instance of the AutomationWorkflow
+        """
+
+        if base_queryset is None:
+            base_queryset = AutomationWorkflow.objects
+
+        try:
+            return base_queryset.select_related("automation__automationworkflow").get(id=workflow_id)
+        except AutomationWorkflow.DoesNotExist:
+            raise AutomationWorkflowDoesNotExist()
+
+    def get_workflows(self, automation: Automation, base_queryset: Optional[QuerySet] = None) -> QuerySet:
+        """
+        Returns all the AutomationWorkflows in the provided automation.
+        """
+
+        if base_queryset is None:
+            base_queryset = AutomationWorkflow.objects.all()
+
+        return base_queryset.filter(automation=automation).select_related(
+            "automation__automationworkflow"
+        )
+
+    def create_workflow(self, automation: Automation, name: str) -> AutomationWorkflow:
+        """
+        Creates a new AutomationWorkflow.
+
+        :param automation: The Automation the workflow belongs to.
+        :return: The newly created AutomationWorkflow instance.
+        """
+
+        last_order = AutomationWorkflow.get_last_order(automation)
+
+        return AutomationWorkflow.objects.create(
+            automation=automation,
+            name=name,
+            order=last_order,
+        )
+
+    def delete_workflow(self, workflow: AutomationWorkflow) -> None:
+        """
+        Deletes the specified AutomationWorkflow.
+
+        :param workflow: The AutomationWorkflow that must be deleted.
+        """
+
+        workflow.delete()
+
+    def update_workflow(self, workflow: AutomationWorkflow, **kwargs) -> AutomationWorkflow:
+        """
+        Updates fields of the provided AutomationWorkflow.
+
+        :param workflow: The AutomationWorkflow that should be updated.
+        :param kwargs: The fields that should be updated with their corresponding values.
+        :return: The updated AutomationWorkflow.
+        """
+
+        for key, value in kwargs.items():
+            setattr(workflow, key, value)
+
+        workflow.save()
+
+        return workflow
+
+    def order_workflows(
+        self, automation: AutomationWorkflow, order: List[int], base_qs=None
+    ) -> List[int]:
+        """
+        Assigns a new order to the workflows in an Automation application.
+        
+        A base_qs can be provided to pre-filter the workflows affected by this change.
+
+        :param automation: The Automation that the workflows belong to.
+        :param order: The new order of the workflows.
+        :param base_qs: A QS that can have filters already applied.
+        :raises AutomationWorkflowNotInAutomation: If the workflow is not part of the provided automation.
+        :return: The new order of the workflows.
+        """
+
+        if base_qs is None:
+            base_qs = AutomationWorkflow.objects.filter(automation=automation)
+
+        try:
+            return AutomationWorkflow.order_objects(base_qs, order)
+        except IdDoesNotExist as error:
+            raise AutomationWorkflowNotInAutomation(error.not_existing_id)
+
+    def duplicate_workflow(
+        self, workflow: AutomationWorkflow, progress_automation: Optional[ChildProgressBuilder] = None
+    ):
+        """
+        Duplicates an existing AutomationWorkflow instance.
+
+        :param workflow: The AutomationWorkflow that is being duplicated.
+        :param progress_automation: A progress object that can be used to report progress.
+        :raises ValueError: When the provided workflow is not an instance of AutomationWorkflow.
+        :return: The duplicated workflow
+        """
+
+        start_progress, export_progress, import_progress = 10, 30, 60
+        progress = ChildProgressBuilder.build(progress_automation, child_total=100)
+        progress.increment(by=start_progress)
+
+        automation = workflow.automation
+
+        exported_workflow = self.export_workflow(workflow)
+
+        # Set a unique name for the workflow to import back as a new one.
+        exported_workflow["name"] = self.find_unused_workflow_name(automation, workflow.name)
+        exported_workflow["order"] = AutomationWorkflow.get_last_order(automation)
+
+        progress.increment(by=export_progress)
+
+        id_mapping = defaultdict(lambda: MirrorDict())
+        id_mapping["automation_workflows"] = MirrorDict()
+
+        new_workflow_clone = self.import_workflow(
+            automation,
+            exported_workflow,
+            progress=progress.create_child_builder(represents_progress=import_progress),
+            id_mapping=id_mapping,
+        )
+
+        return new_workflow_clone
+
+    def find_unused_workflow_name(self, automation: Automation, proposed_name: str) -> str:
+        """
+        Finds an unused name for a workflow in an automation.
+
+        :param automation: The Automation instance that the workflow belongs to.
+        :param proposed_name: The name that is proposed to be used.
+        :return: A unique name to use.
+        """
+
+        existing_workflow_names = list(automation.workflows.values_list("name", flat=True))
+        return find_unused_name([proposed_name], existing_workflow_names, max_length=WORKFLOW_NAME_MAX_LEN)
+
+    def export_workflow(
+        self,
+        workflow: AutomationWorkflow,
+        *args: Any,
+        **kwargs: Any,
+    ) -> List[AutomationWorkflowDict]:
+        """
+        Serializes the given workflow.
+
+        :param workflow: The AutomationWorkflow instance to serialize.
+        :param files_zip: A zip file to store files in necessary.
+        :param storage: Storage to use.
+        :return: The serialized version.
+        """
+
+        return AutomationWorkflowDict(
+            id=workflow.id,
+            name=workflow.name,
+            order=workflow.order,
+        )
+
+    def _ops_count_for_import_workflow(
+        self,
+        serialized_workflows: List[Dict[str, Any]],
+    ) -> int:
+        """
+        Count number of steps for the operation. Used to track task progress.
+        """
+
+        # Return zero for now, since we don't have Triggers and Actions yet.
+        return 0
+
+    def import_workflows(
+        self,
+        automation: Automation,
+        serialized_workflows: List[Dict[str, Any]],
+        id_mapping: Dict[str, Dict[int, int]],
+        files_zip: Optional[ZipFile] = None,
+        storage: Optional[Storage] = None,
+        progress: Optional[ChildProgressBuilder] = None,
+        cache: Optional[Dict[str, any]] = None,
+    ):
+        """
+        Import multiple workflows at once.
+
+        :param automation: The Automation instance the new workflow should belong to.
+        :param serialized_workflows: The serialized version of the workflows.
+        :param id_mapping: A map of old->new id per data type
+            when we have foreign keys that need to be migrated.
+        :param files_zip: Contains files to import if any.
+        :param storage: Storage to get the files from.
+        :return: the newly created instances.
+        """
+
+        if cache is None:
+            cache = {}
+
+        child_total = sum(self._ops_count_for_import_workflow(w) for w in serialized_workflows)
+        progress = ChildProgressBuilder.build(progress, child_total=child_total)
+
+        imported_workflows = []
+        for serialized_workflow in serialized_workflows:
+            workflow_instance = self.import_workflow_only(
+                automation,
+                serialized_workflow,
+                id_mapping,
+                files_zip=files_zip,
+                storage=storage,
+                progress=progress,
+                cache=cache,
+            )
+            imported_workflows.append([workflow_instance, serialized_workflow])
+
+        return [i[0] for i in imported_workflows]
+
+    def import_workflow(
+        self,
+        automation: Automation,
+        serialized_workflow: Dict[str, Any],
+        id_mapping: Dict[str, Dict[int, int]],
+        files_zip: Optional[ZipFile] = None,
+        storage: Optional[Storage] = None,
+        progress: Optional[ChildProgressBuilder] = None,
+        cache: Optional[Dict[str, any]] = None,
+    ) -> AutomationWorkflow:
+        """
+        Creates an instance of AutomationWorkflow using the serialized version
+        previously exported with `.export_workflow'.
+
+        :param automation: The Automation instance the new workflow should belong to.
+        :param serialized_workflow: The serialized version of the AutomationWorkflow.
+        :param id_mapping: A map of old->new id per data type
+            when we have foreign keys that need to be migrated.
+        :param files_zip: Contains files to import if any.
+        :param storage: Storage to get the files from.
+        :return: the newly created instance.
+        """
+
+        return self.import_workflows(
+            automation,
+            [serialized_workflow],
+            id_mapping,
+            files_zip=files_zip,
+            storage=storage,
+            progress=progress,
+            cache=cache,
+        )[0]
+
+    def import_workflow_only(
+        self,
+        automation: Automation,
+        serialized_workflow: Dict[str, Any],
+        id_mapping: Dict[str, Dict[int, int]],
+        progress: Optional[ChildProgressBuilder] = None,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        if "automation_workflows" not in id_mapping:
+            id_mapping["automation_workflows"] = {}
+
+        workflow_instance = AutomationWorkflow.objects.create(
+            automation=automation,
+            name=serialized_workflow["name"],
+            order=serialized_workflow["order"],
+        )
+
+        id_mapping["automation_workflows"][serialized_workflow["id"]] = workflow_instance.id
+
+        progress.increment(state=IMPORT_SERIALIZED_IMPORTING)
+
+        return workflow_instance
---
+++ backend/src/baserow/contrib/automation/workflows/service.py
@@ -0,0 +1,187 @@
+from typing import List, Optional
+
+from django.contrib.auth.models import AbstractUser
+
+from baserow.core.handler import CoreHandler
+from baserow.core.utils import ChildProgressBuilder, extract_allowed
+
+from baserow.contrib.automation.models import Automation, AutomationWorkflow
+from baserow.contrib.automation.workflows.handler import AutomationWorkflowHandler
+from baserow.contrib.automation.operations import OrderAutomationWorkflowsOperationType
+from baserow.contrib.automation.workflows.operations import (
+    CreateWorkflowOperationType,
+    DeleteWorkflowOperationType,
+    DuplicateWorkflowOperationType,
+    ReadWorkflowOperationType,
+    UpdateWorkflowOperationType,
+)
+from baserow.contrib.automation.workflows.signals import (
+    workflow_created,
+    workflow_deleted,
+    workflow_updated,
+    workflows_reordered,
+)
+
+
+class AutomationWorkflowService:
+    def __init__(self):
+        self.handler = AutomationWorkflowHandler()
+
+    def get_workflow(self, user: AbstractUser, workflow_id: int) -> AutomationWorkflow:
+        """
+        Returns a AutomationWorkflow instance by its ID.
+
+        :param user: The user requesting the workflow.
+        :param workflow_id: The ID of the workflow.
+        :return: An instance of AutomationWorkflow.
+        """
+
+        workflow = self.handler.get_workflow(workflow_id)
+
+        CoreHandler().check_permissions(
+            user,
+            ReadWorkflowOperationType.type,
+            workspace=workflow.automation.workspace,
+            context=workflow,
+        )
+
+        return workflow
+
+    def create_workflow(
+        self,
+        user: AbstractUser,
+        automation: Automation,
+        name: str,
+    ) -> AutomationWorkflow:
+        """
+        Returns a new instance of AutomationWorkflow.
+
+        :param user: The user trying to create the workflow.
+        :param automation: The automation the workflow belongs to.
+        :param name: The name of the workflow.
+        :return: The newly created AutomationWorkflow instance.
+        """
+
+        CoreHandler().check_permissions(
+            user,
+            CreateWorkflowOperationType.type,
+            workspace=automation.workspace,
+            context=automation,
+        )
+
+        workflow = self.handler.create_workflow(automation, name)
+
+        workflow_created.send(self, workflow=workflow, user=user)
+
+        return workflow
+
+    def delete_workflow(self, user: AbstractUser, workflow: AutomationWorkflow) -> None:
+        """
+        Deletes the specified workflow.
+
+        :param user: The user trying to delete the workflow.
+        :param workflow: The AutomationWorkflow instance that must be deleted.
+        """
+
+        CoreHandler().check_permissions(
+            user,
+            DeleteWorkflowOperationType.type,
+            workspace=workflow.automation.workspace,
+            context=workflow,
+        )
+
+        workflow_id = workflow.id
+
+        self.handler.delete_workflow(workflow)
+
+        workflow_deleted.send(self, automation=workflow.automation, workflow_id=workflow_id, user=user)
+
+    def update_workflow(self, user: AbstractUser, workflow: AutomationWorkflow, **kwargs) -> AutomationWorkflow:
+        """
+        Updates fields of a workflow.
+
+        :param user: The user trying to update the workflow.
+        :param workflow: The workflow that should be updated.
+        :param kwargs: The fields that should be updated with their corresponding value
+        :return: The updated workflow.
+        """
+
+        CoreHandler().check_permissions(
+            user,
+            UpdateWorkflowOperationType.type,
+            workspace=workflow.automation.workspace,
+            context=workflow,
+        )
+
+        allowed_updates = extract_allowed(
+            kwargs,
+            ["name"],
+        )
+
+        self.handler.update_workflow(workflow, **allowed_updates)
+
+        workflow_updated.send(self, workflow=workflow, user=user)
+
+        return workflow
+
+    def order_workflows(
+        self, user: AbstractUser, automation: Automation, order: List[int]
+    ) -> List[int]:
+        """
+        Assigns a new order to the workflows in an Automation application.
+
+        :param user: The user trying to order the workflows.
+        :param automation: The automation that the workflows belong to.
+        :param order: The new order of the workflows.
+        :return: The new order of the workflows.
+        """
+
+        CoreHandler().check_permissions(
+            user,
+            OrderAutomationWorkflowsOperationType.type,
+            workspace=automation.workspace,
+            context=automation,
+        )
+
+        all_workflows = self.handler.get_workflows(
+            automation, base_queryset=AutomationWorkflow.objects
+        )
+
+        user_workflows = CoreHandler().filter_queryset(
+            user,
+            OrderAutomationWorkflowsOperationType.type,
+            all_workflows,
+            workspace=automation.workspace,
+        )
+
+        full_order = self.handler.order_workflows(automation, order, user_workflows)
+
+        workflows_reordered.send(self, automation=automation, order=full_order, user=user)
+
+        return full_order
+
+    def duplicate_workflow(
+        self,
+        user: AbstractUser,
+        workflow: AutomationWorkflow,
+        progress: Optional[ChildProgressBuilder] = None,
+    ) -> AutomationWorkflow:
+        """
+        Duplicates an existing AutomationWorkflow instance.
+
+        :param user: The user initiating the workflow duplication.
+        :param workflow: The workflow that is being duplicated.
+        :param progress: A ChildProgressBuilder instance that can be used to report progress.
+        :raises ValueError: When the provided workflow is not an instance of AutomationWorkflow.
+        :return: The duplicated workflow.
+        """
+
+        CoreHandler().check_permissions(
+            user, DuplicateWorkflowOperationType.type, workflow.automation.workspace, context=workflow
+        )
+
+        workflow_clone = self.handler.duplicate_workflow(workflow, progress)
+
+        workflow_created.send(self, workflow=workflow_clone, user=user)
+
+        return workflow_clone
