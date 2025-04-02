83dcfa061b1295799fd2f3c1c9c41ca5f7c7b206
Tsering Paljor <tsering@baserow.io>
2025-04-02 15:38:24 +0400

    Add SidebarItemAutomation component and show workflows in sidebar

--- web-frontend/modules/automation/components/sidebar/SidebarComponentAutomation.vue
+++ web-frontend/modules/automation/components/sidebar/SidebarComponentAutomation.vue
@@ -7,13 +7,55 @@
       :highlighted="isAppSelected(application)"
       @selected="selected"
     >
-      <template v-if="isAppSelected(application)" #body></template>
+      <template v-if="isAppSelected(application)" #body>
+        <ul class="tree__subs">
+          <SidebarItemAutomation
+            v-for="workflow in orderedWorkflows"
+            :key="workflow.id"
+            v-sortable="{
+              id: workflow.id,
+              update: orderWorkflows,
+              marginTop: -1.5,
+              enabled: $hasPermission(
+                'workflow.order_workflows',
+                application,
+                application.workspace.id
+              ),
+            }"
+            :automation="application"
+            :workflow="workflow"
+          ></SidebarItemAutomation>
+        </ul>
+
+        <a
+          v-if="
+            $hasPermission(
+              'automation.create_workflow',
+              application,
+              application.workspace.id
+            )
+          "
+          class="tree__sub-add"
+          @click="$refs.createAutomationWorkflowModal.show()"
+        >
+          <i class="tree__sub-add-icon iconoir-plus"></i>
+          {{ $t('sidebarComponentAutomation.createAutomationWorkflow') }}
+        </a>
+        <CreateAutomationWorkflowModal
+          ref="createAutomationWorkflowModal"
+          :automation="application"
+          :workspace="application.workspace"
+        ></CreateAutomationWorkflowModal>
+      </template>
     </SidebarApplication>
   </div>
 </template>
 
 <script>
 import SidebarApplication from '@baserow/modules/core/components/sidebar/SidebarApplication'
+import SidebarItemAutomation from '@baserow/modules/automation/components/sidebar/SidebarItemAutomation'
+import CreateAutomationWorkflowModal from '@baserow/modules/automation/components/workflow/CreateAutomationWorkflowModal'
+
 import { mapGetters } from 'vuex'
 import { notifyIf } from '@baserow/modules/core/utils/error'
 
@@ -21,6 +63,8 @@ export default {
   name: 'SidebarComponentAutomation',
   components: {
     SidebarApplication,
+    SidebarItemAutomation,
+    CreateAutomationWorkflowModal,
   },
   props: {
     application: {
@@ -35,7 +79,11 @@ export default {
   computed: {
     ...mapGetters({
       isAppSelected: 'application/isSelected',
+      getOrderedWorkflows: 'automationWorkflow/getOrderedWorkflows',
     }),
+    orderedWorkflows() {
+      return this.getOrderedWorkflows(this.application)
+    }
   },
   methods: {
     async selected(application) {
@@ -51,6 +99,17 @@ export default {
         if (error.name !== 'NavigationDuplicated') {
           notifyIf(error, 'workspace')
         }
+      }
+    },
+    orderWorkflows(order, oldOrder) {
+      try {
+        this.$store.dispatch('automationWorkflow/order', {
+          automation: this.application,
+          order,
+          oldOrder,
+        })
+      } catch (error) {
+        notifyIf(error, 'automationWorkflow')
       }
     },
   },
