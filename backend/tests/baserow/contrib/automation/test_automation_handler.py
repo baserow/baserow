d6d81b8286c7388d3f53e5110aad119736078499
Tsering Paljor <tsering@baserow.io>
2025-04-04 16:33:39 +0400

    Add tests for automation handler

---
+++ backend/tests/baserow/contrib/automation/test_automation_handler.py
@@ -0,0 +1,17 @@
+import pytest
+
+from baserow.core.exceptions import ApplicationDoesNotExist
+
+from baserow.contrib.automation.handler import AutomationHandler
+
+
+@pytest.mark.django_db
+def test_get_automation(data_fixture):
+    automation = data_fixture.create_automation_application()
+    assert AutomationHandler().get_automation(automation.id).id == automation.id
+
+
+@pytest.mark.django_db
+def test_get_automation_does_not_exist(data_fixture):
+    with pytest.raises(ApplicationDoesNotExist):
+        AutomationHandler().get_automation(9999)
