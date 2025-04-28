2676b3a283650a3c0ce3beb75770621c2749e67d
Tsering Paljor <tsering@baserow.io>
2025-04-28 15:32:19 +0400

    Use singular form of api endpoints

--- backend/src/baserow/contrib/automation/api/nodes/urls.py
+++ backend/src/baserow/contrib/automation/api/nodes/urls.py
@@ -11,22 +11,22 @@ app_name = "baserow.contrib.automation.api.nodes"
 
 urlpatterns = [
     re_path(
-        r"workflows/(?P<workflow_id>[0-9]+)/nodes/$",
+        r"workflow/(?P<workflow_id>[0-9]+)/node/$",
         AutomationNodesView.as_view(),
         name="list",
     ),
     re_path(
-        r"nodes/(?P<node_id>[0-9]+)/$",
+        r"node/(?P<node_id>[0-9]+)/$",
         AutomationNodeView.as_view(),
         name="item",
     ),
     re_path(
-        r"workflows/(?P<workflow_id>[0-9]+)/order/$",
+        r"workflow/(?P<workflow_id>[0-9]+)/order/$",
         OrderAutomationNodesView.as_view(),
         name="order",
     ),
     re_path(
-        r"nodes/(?P<node_id>[0-9]+)/duplicate/$",
+        r"node/(?P<node_id>[0-9]+)/duplicate/$",
         DuplicateAutomationNodeView.as_view(),
         name="duplicate",
     ),
