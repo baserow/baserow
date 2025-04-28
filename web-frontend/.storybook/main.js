b3663317bd3bb24388ddb3b38b715cc68ecf24e1
Jonathan Adeline <jonathan@baserow.io>
2025-04-28 20:22:51 +0400

    fix storybook

--- web-frontend/.storybook/main.js
+++ web-frontend/.storybook/main.js
@@ -3,6 +3,13 @@ const { nuxifyStorybook } = require('../.nuxt-storybook/storybook/main.js')
 module.exports = nuxifyStorybook({
   webpackFinal(config, { configDir }) {
     config.node = { fs: 'empty' }
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@vueuse/core': require.resolve('@vueuse/core'),
+      },
+    }
     return config
   },
   features: { buildStoriesJson: true },
