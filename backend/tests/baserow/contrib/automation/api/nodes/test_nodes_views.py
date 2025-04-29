8b9cb7a8713fb06df62798485087e0038ac4ced4
Tsering Paljor <tsering@baserow.io>
2025-04-29 14:47:05 +0400

    Add tests for ordering nodes

--- backend/tests/baserow/contrib/automation/api/nodes/test_nodes_views.py
+++ backend/tests/baserow/contrib/automation/api/nodes/test_nodes_views.py
@@ -1,13 +1,19 @@
 from django.urls import reverse
 
 import pytest
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+)
 
 from baserow.test_utils.helpers import AnyInt, AnyStr
 
 API_URL_BASE = "api:automation:nodes"
 API_URL_LIST = f"{API_URL_BASE}:list"
 API_URL_ITEM = f"{API_URL_BASE}:item"
+API_URL_ORDER = f"{API_URL_BASE}:order"
 
 
 @pytest.mark.django_db
@@ -122,3 +128,51 @@ def test_get_node_invalid_workflow(api_client, data_fixture):
     )
 
     assert response.status_code == HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+def test_order_nodes(api_client, data_fixture):
+    user, token = data_fixture.create_user_and_token()
+    workflow = data_fixture.create_automation_workflow(user=user)
+    node_1 = data_fixture.create_automation_node(user=user, workflow=workflow)
+    node_2 = data_fixture.create_automation_node(user=user, workflow=workflow)
+
+    list_url = reverse(API_URL_LIST, kwargs={"workflow_id": workflow.id})
+    api_kwargs = {
+        "format": "json",
+        "HTTP_AUTHORIZATION": f"JWT {token}",
+    }
+    response = api_client.get(list_url, **api_kwargs)
+    assert [n["id"] for n in response.json()] == [node_1.id, node_2.id]
+
+    order_url = reverse(API_URL_ORDER, kwargs={"workflow_id": workflow.id})
+    payload = {"node_ids": [node_2.id, node_1.id]}
+    response = api_client.post(order_url, payload, **api_kwargs)
+    assert response.status_code == HTTP_204_NO_CONTENT
+
+    response = api_client.get(list_url, **api_kwargs)
+    assert [n["id"] for n in response.json()] == [node_2.id, node_1.id]
+
+
+@pytest.mark.django_db
+def test_order_nodes_invalid_node(api_client, data_fixture):
+    user, token = data_fixture.create_user_and_token()
+    workflow_1 = data_fixture.create_automation_workflow(user=user)
+
+    # Create a node that belongs to another workflow
+    workflow_2 = data_fixture.create_automation_workflow(user=user)
+    node = data_fixture.create_automation_node(user=user, workflow=workflow_2)
+
+    order_url = reverse(API_URL_ORDER, kwargs={"workflow_id": workflow_1.id})
+    payload = {"node_ids": [node.id]}
+    response = api_client.post(
+        order_url,
+        payload,
+        format="json",
+        HTTP_AUTHORIZATION=f"JWT {token}",
+    )
+    assert response.status_code == HTTP_400_BAD_REQUEST
+    assert response.json() == {
+        "detail": f"The node id {node.id} does not belong to the workflow.",
+        "error": "ERROR_AUTOMATION_NODE_NOT_IN_WORKFLOW",
+    }
