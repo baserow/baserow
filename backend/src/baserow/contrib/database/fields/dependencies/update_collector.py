25c4db3689a3dc1f6839ee2c5a84aac9e42f9f34
Bram Wiepjes <bramw@protonmail.com>
2025-05-20 21:55:29 +0200

    respect deleted m2m relationships

--- backend/src/baserow/contrib/database/fields/dependencies/update_collector.py
+++ backend/src/baserow/contrib/database/fields/dependencies/update_collector.py
@@ -17,6 +17,8 @@ from baserow.contrib.database.table.models import Table
 from baserow.contrib.database.table.signals import table_updated
 from baserow.core.db import UpdatableCTEWith
 
+from .utils import include_rows_connected_to_deleted_m2m_relationships
+
 StartingRowIdsType = Optional[List[int]]
 
 
@@ -25,10 +27,15 @@ class CTECollector:
     @TODO docs.
     """
 
-    def __init__(self, starting_row_ids: Optional[List[int]] = None):
+    def __init__(
+        self,
+        starting_row_ids: Optional[List[int]] = None,
+        deleted_m2m_rels_per_link_field: Optional[Dict[int, Set[int]]] = None,
+    ):
         self.cte = defaultdict(dict)
         self.starting_row_ids = starting_row_ids
         self.last_path_to_starting_table = None
+        self._deleted_m2m_rels_per_link_field = deleted_m2m_rels_per_link_field
 
     def has(self, table_id, name: str):
         return name in self.cte[table_id]
@@ -94,10 +101,16 @@ class CTECollector:
                     path_to_starting_table_row_id_column += (
                         f"{link_row_field.db_column}__"
                     )
+                path_to_starting_table_row_id_column += "id__in"
+
+                filter_for_rows_connected_to_starting_row = Q(
+                    **{path_to_starting_table_row_id_column: starting_row_ids}
+                ) | include_rows_connected_to_deleted_m2m_relationships(
+                    self._deleted_m2m_rels_per_link_field,
+                    path_to_starting_table,
+                )
                 cte_queryset = cte_queryset.filter(
-                    **{
-                        f"{path_to_starting_table_row_id_column}id__in": starting_row_ids
-                    }
+                    filter_for_rows_connected_to_starting_row
                 )
                 cte_with.set_source_queryset(cte_queryset)
 
@@ -268,7 +281,7 @@ class PathBasedUpdateStatementCollector:
 
             filter_for_rows_connected_to_starting_row = Q(
                 **{path_to_starting_table_id_column: starting_row_ids}
-            ) | self._include_rows_connected_to_deleted_m2m_relationships(
+            ) | include_rows_connected_to_deleted_m2m_relationships(
                 deleted_m2m_rels_per_link_field,
                 path_to_starting_table,
             )
@@ -317,49 +330,6 @@ class PathBasedUpdateStatementCollector:
             updated_rows = update_queryset.update(**self.update_statements)
 
         return updated_rows
-
-    def _include_rows_connected_to_deleted_m2m_relationships(
-        self,
-        deleted_m2m_rels_per_link_field: Dict[int, Set[int]],
-        path_to_starting_table: List[LinkRowField],
-    ):
-        """
-        If a row or batch of rows have been updated breaking their link row connections
-        with other rows, we need to ensure that those other rows are still updated.
-        We can't just join back to the starting row id as that m2m relation has been
-        deleted by now. Instead the provided dict contains per link field which rows
-        have had their connections deleted. This method then constructs a Q filter that
-        ensures this UPDATE statement will also update those rows as they need to
-        change their values because a connection has been removed for them.
-
-        :return: A filter including any rows which previously were connected to the
-            starting row.
-        """
-
-        if deleted_m2m_rels_per_link_field is None or not path_to_starting_table:
-            return Q()
-
-        # The first link row field in the path will be a link row field not in the
-        # starting table, but which leads to the starting table. However the
-        # deleted_m2m_rels_per_link_field is a dictionary per link field of rows in
-        # the table it links to which have had their connections removed. Hence we
-        # need to use the link row field in the starting table to lookup the deleted
-        # row ids in the table after the starting table.
-        link_row_field_in_starting_table: int = cast(
-            int, path_to_starting_table[-1].link_row_related_field_id
-        )
-        filters = Q()
-        if link_row_field_in_starting_table in deleted_m2m_rels_per_link_field:
-            path_to_table_after_starting_table = "".join(
-                [p.db_column + "__" for p in path_to_starting_table[:-1]]
-            )
-
-            row_ids = deleted_m2m_rels_per_link_field[link_row_field_in_starting_table]
-            filter_kwargs_forcing_update_for_row_with_deleted_rels = {
-                f"{path_to_table_after_starting_table}id__in": row_ids
-            }
-            filters |= Q(**filter_kwargs_forcing_update_for_row_with_deleted_rels)
-        return filters
 
 
 class FieldUpdatesTracker(defaultdict):
@@ -440,7 +410,9 @@ class FieldUpdateCollector:
         self.cte_collector = self._init_cte_collector()
 
     def _init_cte_collector(self):
-        return CTECollector(self._starting_row_ids)
+        return CTECollector(
+            self._starting_row_ids, self._deleted_m2m_rels_per_link_field
+        )
 
     def _init_update_statement_collector(self):
         return PathBasedUpdateStatementCollector(
