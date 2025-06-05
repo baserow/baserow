d27eb2d3b78af2042d5295c9b4c991c11d33003b
Tsering Paljor <tsering@baserow.io>
2025-06-05 17:23:47 +0400

    Remove unused errors

--- backend/src/baserow/contrib/automation/api/workflows/errors.py
+++ backend/src/baserow/contrib/automation/api/workflows/errors.py
@@ -29,15 +29,3 @@ ERROR_AUTOMATION_NODE_NOT_IN_WORKFLOW = (
     HTTP_400_BAD_REQUEST,
     "The node id {e.node_id} does not belong to the workflow.",
 )
-
-ERROR_AUTOMATION_WORKFLOW_IS_EMPTY = (
-    "ERROR_AUTOMATION_WORKFLOW_IS_EMPTY",
-    HTTP_400_BAD_REQUEST,
-    "The workflow {e.workflow_id} contains no nodes, start by adding a trigger.",
-)
-
-ERROR_AUTOMATION_NODE_MISCONFIGURED_SERVICE = (
-    "ERROR_AUTOMATION_NODE_MISCONFIGURED_SERVICE",
-    HTTP_400_BAD_REQUEST,
-    "The node {e.node_id} has a misconfigured service.",
-)
