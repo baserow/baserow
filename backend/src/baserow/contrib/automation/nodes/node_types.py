87e4d3744d6a7764eaea812aac6260a0bca7ff88
Tsering Paljor <tsering@baserow.io>
2025-06-11 17:14:31 +0400

    Fix typo: update_fields instead of updated_fields

--- backend/src/baserow/contrib/automation/nodes/node_types.py
+++ backend/src/baserow/contrib/automation/nodes/node_types.py
@@ -98,7 +98,7 @@ class AutomationNodeTriggerType(AutomationNodeType):
             )
             if workflow.allow_test_run_until:
                 workflow.allow_test_run_until = None
-                workflow.save(updated_fields=["allow_test_run_until"])
+                workflow.save(update_fields=["allow_test_run_until"])
 
     def after_register(self):
         service_type_registry.get(self.service_type).start_listening(self.on_event)
