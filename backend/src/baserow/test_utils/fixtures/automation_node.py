0f85a4937735889e1ed1e380ba4dc5a84344a1ce
peter_baserow <peter@baserow.io>
2025-06-18 15:28:33 +0100

    Create the service in create_automation_node by getting the model_class from the service type.

--- backend/src/baserow/test_utils/fixtures/automation_node.py
+++ backend/src/baserow/test_utils/fixtures/automation_node.py
@@ -1,18 +1,13 @@
 from baserow.contrib.automation.nodes.handler import AutomationNodeHandler
 from baserow.contrib.automation.nodes.models import AutomationNode
 from baserow.contrib.automation.nodes.node_types import (
-    AutomationNodeTriggerType,
     LocalBaserowCreateRowNodeType,
     LocalBaserowDeleteRowNodeType,
     LocalBaserowRowsCreatedNodeTriggerType,
     LocalBaserowUpdateRowNodeType,
 )
 from baserow.contrib.automation.nodes.registries import automation_node_type_registry
-from baserow.contrib.integrations.local_baserow.models import (
-    LocalBaserowDeleteRow,
-    LocalBaserowRowsCreated,
-    LocalBaserowUpsertRow,
-)
+from baserow.core.services.registries import service_type_registry
 
 
 class AutomationNodeFixtures:
@@ -33,12 +28,10 @@ class AutomationNodeFixtures:
 
         if "service" not in kwargs:
             service_kwargs = kwargs.pop("service_kwargs", {})
-            service_model = LocalBaserowUpsertRow
-            if issubclass(node_type.__class__, AutomationNodeTriggerType):
-                service_model = LocalBaserowRowsCreated
-            elif issubclass(node_type.__class__, LocalBaserowDeleteRowNodeType):
-                service_model = LocalBaserowDeleteRow
-            kwargs["service"] = self.create_service(service_model, **service_kwargs)
+            service_type = service_type_registry.get(node_type.service_type)
+            kwargs["service"] = self.create_service(
+                service_type.model_class, **service_kwargs
+            )
 
         if "order" not in kwargs:
             kwargs["order"] = AutomationNode.get_last_order(workflow)
