6a7f65932e516ae90575c7a459a01405a46e5505
peter_baserow <peter@baserow.io>
2025-06-19 09:58:54 +0100

    Bump the migration ID

--- backend/src/baserow/contrib/automation/migrations/0009_corehttprequestactionnode.py
+++ backend/src/baserow/contrib/automation/migrations/0009_corehttprequestactionnode.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("automation", "0007_localbaserowupdaterowactionnode"),
+        ("automation", "0008_localbaserowdeleterowactionnode"),
     ]
 
     operations = [
