6ec48b93e34c3002bfdd5c30065c84a7e2f2b40e
Tsering Paljor <tsering@baserow.io>
2025-06-30 15:07:59 +0400

    Ensure published_on is a string

--- backend/src/baserow/contrib/automation/api/workflows/serializers.py
+++ backend/src/baserow/contrib/automation/api/workflows/serializers.py
@@ -33,7 +33,7 @@ class AutomationWorkflowSerializer(serializers.ModelSerializer):
     @extend_schema_field(OpenApiTypes.STR)
     def get_published_on(self, obj):
         published_workflow = AutomationWorkflowHandler().get_published_workflow(obj.id)
-        return published_workflow.created_on if published_workflow else None
+        return str(published_workflow.created_on) if published_workflow else None
 
 
 class CreateAutomationWorkflowSerializer(serializers.ModelSerializer):
