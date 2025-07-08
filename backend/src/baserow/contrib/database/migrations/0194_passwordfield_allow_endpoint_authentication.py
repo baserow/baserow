44512f9ade70e0b69812f83c572706692094c5a2
Davide Silvestri <davide@baserow.io>
2025-07-08 13:58:09 +0200

    Fix migration conflict

--- backend/src/baserow/contrib/database/migrations/0194_passwordfield_allow_endpoint_authentication.py
+++ backend/src/baserow/contrib/database/migrations/0194_passwordfield_allow_endpoint_authentication.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("database", "0191_field_db_index"),
+        ("database", "0193_fieldconstraint"),
     ]
 
     operations = [
