515a573c478d0ca60a09c1ead03097cce993714d
Tsering Paljor <tsering@baserow.io>
2025-07-14 16:17:51 +0400

    Add handling to node and workflow so that history instances are created

--- backend/src/baserow/contrib/automation/workflows/tasks.py
+++ backend/src/baserow/contrib/automation/workflows/tasks.py
@@ -1,11 +1,15 @@
 from typing import Dict, List, Optional, Union
 
+from django.utils import timezone
+
 from baserow.config.celery import app
 from baserow.contrib.automation.automation_dispatch_context import (
     AutomationDispatchContext,
 )
+from baserow.contrib.automation.history.constants import HistoryStatusChoices
 from baserow.contrib.automation.workflows.runner import AutomationWorkflowRunner
 from baserow.core.db import atomic_with_retry_on_deadlock
+from baserow.core.services.exceptions import DispatchException
 
 
 @app.task(bind=True, queue="automation_workflow")
@@ -13,9 +17,35 @@ from baserow.core.db import atomic_with_retry_on_deadlock
 def run_workflow(
     self, workflow_id: int, event_payload: Optional[Union[Dict, List[Dict]]]
 ):
+    from baserow.contrib.automation.history.handler import AutomationHistoryHandler
     from baserow.contrib.automation.workflows.handler import AutomationWorkflowHandler
 
     workflow = AutomationWorkflowHandler().get_workflow(workflow_id)
     dispatch_context = AutomationDispatchContext(workflow, event_payload)
+    history_handler = AutomationHistoryHandler()
 
-    AutomationWorkflowRunner().run(workflow, dispatch_context)
+    try:
+        AutomationWorkflowRunner().run(workflow, dispatch_context)
+    except DispatchException as e:
+        history_message = str(e)
+        history_status = HistoryStatusChoices.ERROR
+    except Exception as e:
+        # For unexpected errors, store a generic message in history
+        history_message = f"Unexpected error while running workflow {workflow_id}"
+        history_status = HistoryStatusChoices.ERROR
+
+        raise e
+    else:
+        history_message = ""
+        history_status = HistoryStatusChoices.SUCCESS
+    finally:
+        history_handler.create_workflow_history(
+            workflow,
+            completed_on=timezone.now(),
+            status=history_status,
+            message=history_message,
+        )
+
+        if workflow.allow_test_run_until:
+            workflow.allow_test_run_until = None
+            workflow.save(update_fields=["allow_test_run_until"])
