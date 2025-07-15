4bb95da05a40e2430ccba4093292375f7bba043e
Tsering Paljor <tsering@baserow.io>
2025-07-15 16:54:44 +0400

    Add a basic history side panel

--- web-frontend/modules/automation/components/workflow/sidePanels/HistorySidePanel.vue
+++ web-frontend/modules/automation/components/workflow/sidePanels/HistorySidePanel.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="empty-automation-side-panel-state">
+  <div
+    v-if="!workflowHistoryItems.length"
+    class="empty-automation-side-panel-state"
+  >
     <i
       class="baserow-icon-automation empty-automation-side-panel-state__icon"
     ></i>
-    <h4>{{ $t('emptyHistorySidePanelState.title') }}</h4>
+    <h4>{{ $t('historySidePanel.noRunsTitle') }}</h4>
     <p class="margin-top-0">
-      {{ $t('emptyHistorySidePanelState.description') }}
+      {{ $t('historySidePanel.noRunsDescription') }}
     </p>
   </div>
+  <div v-else>
+    <HistorySection
+      v-for="item in workflowHistoryItems"
+      :key="item.id"
+      :item="item"
+    />
+  </div>
 </template>
+
+<script setup>
+import { computed, useStore } from '@nuxtjs/composition-api'
+import HistorySection from '@baserow/modules/automation/components/workflow/sidePanels/HistorySection'
+const store = useStore()
+
+const workflowHistoryItems = computed(() => {
+  const history = store.getters['automationHistory/getWorkflowHistory']()
+  return history?.results || []
+})
+</script>
