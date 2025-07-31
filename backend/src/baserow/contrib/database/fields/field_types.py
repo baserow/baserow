b2488078c2e6be80ca93bce84f86e5f8b9c3da7b
Przemyslaw Kukulski <przemyslaw@baserow.io>
2025-07-31 15:38:44 +0200

    Review tweaks

--- backend/src/baserow/contrib/database/fields/field_types.py
+++ backend/src/baserow/contrib/database/fields/field_types.py
@@ -4736,7 +4736,8 @@ class MultipleSelectFieldType(
         return [
             index
             for index, option in enumerate(select_options)
-            if option["id"] in default
+            if option.get("id") in default
+            or option.get(UPSERT_OPTION_DICT_KEY) in default
         ]
 
     def get_options_by_index(self, field, index):
