16c31d756e7304d534f1b2ec4aa39828ea57f6dc
Bram Wiepjes <bramw@protonmail.com>
2025-09-07 23:29:30 +0200

    combined public view and restricted query

--- backend/src/baserow/contrib/database/views/handler.py
+++ backend/src/baserow/contrib/database/views/handler.py
@@ -3409,48 +3409,6 @@ class ViewHandler(metaclass=baserow_trace_methods(tracer)):
         )
         return created_row
 
-    def get_public_views_row_checker(
-        self,
-        table,
-        model,
-        only_include_views_which_want_realtime_events,
-        updated_field_ids=None,
-    ):
-        """
-        Returns a FilteredViewRowChecker object which will have precalculated
-        information about the public views in the provided table to aid with quickly
-        checking which views a row in that table is visible in. If you will be updating
-        the row and reusing the checker you must provide an iterable of the field ids
-        that you will be updating in the row, otherwise the checker will cache the
-        first check per view/row.
-
-        :param table: The table the row is in.
-        :param model: The model of the table including all fields.
-        :param only_include_views_which_want_realtime_events: If True will only look
-            for public views where
-            ViewType.when_shared_publicly_requires_realtime_events is True.
-        :param updated_field_ids: An optional iterable of field ids which will be
-            updated on rows passed to the checker. If the checker is used on the same
-            row multiple times and that row has been updated it will return invalid
-            results unless you have correctly populated this argument.
-        :return: A list of non-specific public view instances.
-        """
-
-        from .row_checker import FilteredViewRowChecker
-
-        queryset = (
-            table.view_set.filter(public=True)
-            .select_related("table")
-            .prefetch_related("viewfilter_set", "filter_groups", "table__field_set")
-            .all()
-        )
-        return FilteredViewRowChecker(
-            model,
-            queryset,
-            only_include_views_which_want_realtime_events,
-            updated_field_ids,
-        )
-
     def restrict_row_for_view(
         self, view: View, serialized_row: Dict[str, Any]
     ) -> Dict[Any, Any]:
