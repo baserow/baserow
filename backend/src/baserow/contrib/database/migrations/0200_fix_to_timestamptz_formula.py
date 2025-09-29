8bbda178394824895c891c9d3ab417f6ae3d2414
Bram Wiepjes <bramw@protonmail.com>
2025-09-29 20:48:52 +0200

    fix applying pg function in order in tests

--- backend/src/baserow/contrib/database/migrations/0200_fix_to_timestamptz_formula.py
+++ backend/src/baserow/contrib/database/migrations/0200_fix_to_timestamptz_formula.py
@@ -1,41 +1,9 @@
 from django.db import migrations
 
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("database", "0199_field_rules"),
-    ]
-
-    operations = [
-        migrations.RunSQL(
-            (
-                """
-create or replace function try_cast_to_date_tz(
-    p_in text,
-    p_format text,
-    p_timezone text
-)
-returns timestamptz
-as
-$$
-declare
-    tstamp timestamp := null;
-begin
-    begin
-        tstamp := to_timestamp(p_in, p_format);
-        return (tstamp AT TIME ZONE p_timezone);
-    exception when others then
-        return null;
-    end;
-end;
-$$
-language plpgsql;
-"""
-            ),
-            # old function from
-            # `src/baserow/contrib/database/migrations/0106_add_to_timestamptz_formula.py`
-            (
-                """
+# The old function must be listed added first in the file because the
+# `iter_formula_pgsql_functions` method extracts that one first. Added the new first
+# second, makes sure that that version is executed last.
+old_function = """
 create or replace function try_cast_to_date_tz(
     p_in text,
     p_format text,
@@ -60,7 +28,42 @@ begin
 end;
 $$
     language plpgsql;
-                """
-            ),
+"""
+
+new_function = """
+create or replace function try_cast_to_date_tz(
+    p_in text,
+    p_format text,
+    p_timezone text
+)
+returns timestamptz
+as
+$$
+declare
+    tstamp timestamp := null;
+begin
+    begin
+        tstamp := to_timestamp(p_in, p_format);
+        return (tstamp AT TIME ZONE p_timezone);
+    exception when others then
+        return null;
+    end;
+end;
+$$
+language plpgsql;
+"""
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("database", "0199_field_rules"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            new_function,
+            # old function from
+            # `src/baserow/contrib/database/migrations/0106_add_to_timestamptz_formula.py`
+            old_function,
         )
     ]
