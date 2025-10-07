4e295bcb98a02b0571dd30e528c9bc57d16aa488
Bram Wiepjes <bramw@protonmail.com>
2025-10-07 15:29:34 +0200

    use single command

--- web-frontend/docker/docker-entrypoint.sh
+++ web-frontend/docker/docker-entrypoint.sh
@@ -93,8 +93,7 @@ case "$1" in
     nuxt-build-and-start)
       startup_plugin_setup
       setup_additional_modules
-      exec ./node_modules/.bin/nuxt build
-      exec ./node_modules/.bin/nuxt start --hostname "${BASEROW_WEBFRONTEND_BIND_ADDRESS:-0.0.0.0}" --port "$BASEROW_WEBFRONTEND_PORT" "${@:2}"
+      exec ./node_modules/.bin/nuxt build && exec ./node_modules/.bin/nuxt start --hostname "${BASEROW_WEBFRONTEND_BIND_ADDRESS:-0.0.0.0}" --port "$BASEROW_WEBFRONTEND_PORT" "${@:2}"
     ;;
     nuxt-local)
       startup_plugin_setup
