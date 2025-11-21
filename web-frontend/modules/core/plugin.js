eeeff3d4a646e161b338d356a555ccef489162ca
Tsering Paljor <tsering@baserow.io>
2025-11-21 15:14:07 +0400

    Add strip()

--- web-frontend/modules/core/plugin.js
+++ web-frontend/modules/core/plugin.js
@@ -136,6 +136,7 @@ import {
   RuntimeJoin,
   RuntimeSplit,
   RuntimeIsEmpty,
+  RuntimeStrip,
 } from '@baserow/modules/core/runtimeFormulaTypes'
 
 import priorityBus from '@baserow/modules/core/plugins/priorityBus'
@@ -325,6 +326,7 @@ export default (context, inject) => {
   registry.register('runtimeFormulaFunction', new RuntimeJoin(context))
   registry.register('runtimeFormulaFunction', new RuntimeSplit(context))
   registry.register('runtimeFormulaFunction', new RuntimeIsEmpty(context))
+  registry.register('runtimeFormulaFunction', new RuntimeStrip(context))
 
   registry.register('roles', new AdminRoleType(context))
   registry.register('roles', new MemberRoleType(context))
