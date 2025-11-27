7d36dfb93ae790877fa01008cd06a04c84a937c6
Bram Wiepjes <bramw@protonmail.com>
2025-11-27 20:11:45 +0100

    Remove ping/pong code

--- backend/src/baserow/ws/consumers.py
+++ backend/src/baserow/ws/consumers.py
@@ -158,8 +158,6 @@ class CoreConsumer(AsyncJsonWebsocketConsumer):
         Processes incoming messages.
         """
 
-        if content.get("type") == "ping":
-            await self.send_json({"type": "pong"})
         if "page" in content:
             await self._add_page_scope(content)
         if "remove_page" in content:
