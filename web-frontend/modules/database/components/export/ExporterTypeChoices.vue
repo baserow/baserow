c82ac19914092569e9ebba4fdd063d0e94c9c1b2
Bram Wiepjes <bramw@protonmail.com>
2025-12-10 20:39:03 +0100

    Fix ExporterTypeChoices

--- web-frontend/modules/database/components/export/ExporterTypeChoices.vue
+++ web-frontend/modules/database/components/export/ExporterTypeChoices.vue
@@ -9,7 +9,7 @@
         v-for="exporterType in exporterTypes"
         :key="exporterType.type"
         :exporter-type="exporterType"
-        :active="value !== null && value === exporterType.type"
+        :active="modelValue !== null && modelValue === exporterType.type"
         :disabled="loading"
         :database="database"
         @selected="switchToExporterType(exporterType.type)"
@@ -53,7 +53,7 @@ export default {
       required: true,
       type: Array,
     },
-    value: {
+    modelValue: {
       required: false,
       type: String,
       default: null,
@@ -69,7 +69,7 @@ export default {
         return
       }
 
-      this.$emit('input', exporterType)
+      this.$emit('update:modelValue', exporterType)
     },
   },
 }
