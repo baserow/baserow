7bc71319a8fb3f0a121556f221eb511b6438b445
Tsering Paljor <tsering@baserow.io>
2025-12-18 10:15:24 +0400

    Allow >= to compare any type

--- backend/tests/baserow/core/formula/test_runtime_formula_types.py
+++ backend/tests/baserow/core/formula/test_runtime_formula_types.py
@@ -624,12 +624,46 @@ def test_runtime_less_than_validate_number_of_args(args, expected):
         ([{}, "a"], None),
         ([1, "a"], None),
         (["a", 1], None),
+        (
+            [
+                date(2025, 12, 17),
+                date(2025, 12, 18),
+            ],
+            False,
+        ),
+        (
+            [
+                date(2025, 12, 20),
+                date(2025, 12, 19),
+            ],
+            True,
+        ),
+        (
+            [
+                datetime(year=2025, month=11, day=6, hour=12, minute=29),
+                datetime(year=2025, month=11, day=6, hour=12, minute=30),
+            ],
+            False,
+        ),
+        (
+            [
+                datetime(year=2025, month=11, day=6, hour=12, minute=30),
+                datetime(year=2025, month=11, day=6, hour=12, minute=30),
+            ],
+            True,
+        ),
     ],
 )
 def test_runtime_greater_than_or_equal_execute(args, expected):
     parsed_args = RuntimeGreaterThanOrEqual().parse_args(args)
-    result = RuntimeGreaterThanOrEqual().execute({}, parsed_args)
-    assert result == expected
+
+    if expected is None:
+        with pytest.raises(TypeError) as e:
+            RuntimeGreaterThanOrEqual().execute({}, parsed_args)
+        assert f"'>=' not supported between instances of" in str(e)
+    else:
+        result = RuntimeGreaterThanOrEqual().execute({}, parsed_args)
+        assert result == expected
 
 
 @pytest.mark.parametrize(
