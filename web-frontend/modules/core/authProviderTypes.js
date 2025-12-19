050d2e76e78ec12be609c85d639fb79705afede7
Jonathan Adeline <jonathan@baserow.io>
2025-12-19 11:08:33 +0400

    Migrate auth provider page

--- web-frontend/modules/core/authProviderTypes.js
+++ web-frontend/modules/core/authProviderTypes.js
@@ -1,5 +1,5 @@
 import { Registerable } from '@baserow/modules/core/registry'
-import PasswordAuthIcon from '@baserow/modules/core/assets/images/providers/Key.svg'
+import PasswordAuthIcon from '@baserow/modules/core/assets/images/providers/Key.svg?url'
 
 /**
  * Base class for authorization provider types
