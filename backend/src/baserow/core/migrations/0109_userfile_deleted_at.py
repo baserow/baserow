cbfcbcc2505ea20a6e18890d08aa5d330ca91ced
Bram Wiepjes <bramw@protonmail.com>
2025-12-23 21:04:26 +0100

    fixed migration

--- backend/src/baserow/core/migrations/0109_userfile_deleted_at.py
+++ backend/src/baserow/core/migrations/0109_userfile_deleted_at.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("core", "0107_twofactorauthprovidermodel_totpauthprovidermodel_and_more"),
+        ("core", "0108_alter_userfile_unique"),
     ]
 
     operations = [
