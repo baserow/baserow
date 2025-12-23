b5eeff8e8b15c743ed1a4a2a4031dd721c97b149
Jonathan Adeline <jonathan@baserow.io>
2025-12-23 16:01:56 +0400

    fix server warning

--- web-frontend/modules/core/pages/index.vue
+++ web-frontend/modules/core/pages/index.vue
@@ -1,9 +1,9 @@
-<script>
-export default {
-  async setup() {
-    const store = useNuxtApp().$store
-    const name = store.getters['auth/isAuthenticated'] ? 'dashboard' : 'login'
-    await navigateTo({ name })
-  },
-}
+<template>
+  <div />
+</template>
+
+<script setup>
+const store = useNuxtApp().$store
+const name = store.getters['auth/isAuthenticated'] ? 'dashboard' : 'login'
+await navigateTo({ name })
 </script>
