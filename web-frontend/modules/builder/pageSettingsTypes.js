573a1636e85a47f158d2255c81dc9c2d34317339
Tsering Paljor <tsering@baserow.io>
2025-12-26 11:39:41 +0400

    Fix opening of page settings modal

--- web-frontend/modules/builder/pageSettingsTypes.js
+++ web-frontend/modules/builder/pageSettingsTypes.js
@@ -1,6 +1,10 @@
+import { markRaw } from 'vue'
 import { Registerable } from '@baserow/modules/core/registry'
-import PageSettings from '@baserow/modules/builder/components/page/settings/PageSettings'
-import PageVisibilitySettings from '@baserow/modules/builder/components/page/settings/PageVisibilitySettings'
+import PageSettingsComponent from '@baserow/modules/builder/components/page/settings/PageSettings'
+import PageVisibilitySettingsComponent from '@baserow/modules/builder/components/page/settings/PageVisibilitySettings'
+
+const PageSettings = markRaw(PageSettingsComponent)
+const PageVisibilitySettings = markRaw(PageVisibilitySettingsComponent)
 
 export class PageSettingType extends Registerable {
   static getType() {
