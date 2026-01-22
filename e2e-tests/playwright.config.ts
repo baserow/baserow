766545f54f129cf438d89eeeb1ff31b5debd75bb
Jeremie Pardou <571533+jrmi@users.noreply.github.com>
2026-01-22 09:49:34 +0100

    Fix e2e test report collection job

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -33,8 +33,7 @@ const config: PlaywrightTestConfig = {
   retries: !!process.env.CI ? 3 : 0,
   workers: !!process.env.CI ? 1 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // TODO MIG reporter: !!process.env.CI ? [["blob"], ["html"]] : "list",
-  reporter: !!process.env.CI ? [["blob"], ["html"], ["dot"]] : "list",
+  reporter: !!process.env.CI ? [["blob"], ["html"]] : "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
