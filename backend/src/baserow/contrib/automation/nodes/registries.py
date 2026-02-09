90d01e9ea20695845e0b7afecee4fd4ad320e94f
peter_baserow <peter@baserow.io>
2026-02-09 14:36:30 +0000

    Extract most of the AutomationWorkflowGraphHandler code into a new BaseGraphHandler, which is even more generic. This will allow us to create a core graph handler which both application and automation builders can inherit from.

--- backend/src/baserow/contrib/automation/nodes/registries.py
+++ backend/src/baserow/contrib/automation/nodes/registries.py
@@ -10,8 +10,9 @@ from baserow.contrib.automation.nodes.exceptions import (
     AutomationNodeNotReplaceable,
 )
 from baserow.contrib.automation.nodes.models import AutomationNode
-from baserow.contrib.automation.nodes.types import AutomationNodeDict, NodePositionType
+from baserow.contrib.automation.nodes.types import AutomationNodeDict
 from baserow.contrib.automation.workflows.models import AutomationWorkflow
+from baserow.core.graph.types import GraphPointPositionType
 from baserow.core.integrations.models import Integration
 from baserow.core.registry import (
     CustomFieldsRegistryMixin,
@@ -86,7 +87,7 @@ class AutomationNodeType(
         self,
         node: AutomationNode,
         reference_node: AutomationNode | None,
-        position: NodePositionType,
+        position: GraphPointPositionType,
         output: str,
     ):
         """Called before the node is moved."""
@@ -95,7 +96,7 @@ class AutomationNodeType(
         self,
         workflow: AutomationWorkflow,
         reference_node: AutomationNode | None,
-        position: NodePositionType,
+        position: GraphPointPositionType,
         output: str,
     ):
         """
