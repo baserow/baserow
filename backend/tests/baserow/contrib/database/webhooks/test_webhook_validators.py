0260ac42f3af3920b80bbfcfe3c3eaf722396d14
Petr Stribny <petr@stribny.name>
2026-02-09 18:32:50 +0100

    Mock socket.connect properly

--- backend/tests/baserow/contrib/database/webhooks/test_webhook_validators.py
+++ backend/tests/baserow/contrib/database/webhooks/test_webhook_validators.py
@@ -5,19 +5,33 @@ from django.core.exceptions import ValidationError
 from django.test import override_settings
 
 import pytest
-import responses
 
+import advocate.connection as advocate_connection
 from baserow.contrib.database.webhooks.validators import url_validator
 
 URL_BLACKLIST_ONLY_ALLOWING_GOOGLE_WEBHOOKS = re.compile(r"(?!(www\.)?google\.com).*")
 
 
-@responses.activate
-def test_advocate_blocks_internal_address():
-    responses.add(responses.GET, "https://1.1.1.1/", status=200)
-    responses.add(responses.GET, "https://2.2.2.2/", status=200)
-    responses.add(responses.GET, "http://127.0.0.1/", status=200)
+class _DummySocket:
+    def settimeout(self, *args, **kwargs):
+        pass
 
+    def connect(self, *args, **kwargs):
+        # Never do real outbound network in unit tests.
+        return None
+
+    def close(self):
+        pass
+
+
+@pytest.fixture(autouse=True)
+def _disable_real_network(monkeypatch):
+    monkeypatch.setattr(
+        advocate_connection.socket, "socket", lambda *args, **kwargs: _DummySocket()
+    )
+
+
+def test_advocate_blocks_internal_address():
     # This request should go through
     url_validator("https://1.1.1.1/")
 
@@ -26,12 +40,7 @@ def test_advocate_blocks_internal_address():
         url_validator("http://127.0.0.1/")
 
 
-@responses.activate
 def test_advocate_blocks_invalid_urls():
-    responses.add(responses.GET, "https://1.1.1.1/", status=200)
-    responses.add(responses.GET, "https://2.2.2.2/", status=200)
-    responses.add(responses.GET, "http://127.0.0.1/", status=200)
-
     # This request should go through
     url_validator("https://1.1.1.1/")
 
@@ -44,12 +53,8 @@ def test_advocate_blocks_invalid_urls():
     assert exec_info.value.code == "invalid_url"
 
 
-@responses.activate
 @override_settings(BASEROW_WEBHOOKS_IP_WHITELIST=[ip_network("127.0.0.1/32")])
 def test_advocate_whitelist_rules():
-    responses.add(responses.GET, "http://127.0.0.1/", status=200)
-    responses.add(responses.GET, "http://10.0.0.1/", status=200)
-
     # This request should go through
     url_validator("http://127.0.0.1/")
 
@@ -59,13 +64,8 @@ def test_advocate_whitelist_rules():
     assert exec_info.value.code == "invalid_url"
 
 
-@responses.activate
 @override_settings(BASEROW_WEBHOOKS_IP_BLACKLIST=[ip_network("1.1.1.1/32")])
 def test_advocate_blacklist_rules():
-    responses.add(responses.GET, "https://1.1.1.1", status=200)
-    responses.add(responses.GET, "http://127.0.0.1/", status=200)
-    responses.add(responses.GET, "https://2.2.2.2/", status=200)
-
     # This request should not go through
     with pytest.raises(ValidationError, match="Invalid URL") as exec_info:
         url_validator("https://1.1.1.1/")
@@ -77,20 +77,13 @@ def test_advocate_blacklist_rules():
     assert exec_info.value.code == "invalid_url"
 
     # This request should still go through
-    url_validator("https://2.2.2.2/")
+    url_validator("http://2.2.2.2/")
 
 
-@responses.activate
 @override_settings(
     BASEROW_WEBHOOKS_URL_REGEX_BLACKLIST=[re.compile(r"(?:www\.?)?google.com")]
 )
 def test_hostname_blacklist_rules():
-    responses.add(responses.GET, "https://google.com", status=200)
-    responses.add(responses.GET, "http://1.1.1.1", status=200)
-
-    # The responses stub implemenation of socket.getaddrinfo is incorrect and doesn't
-    # return an IP causing advocate to fail, instead we patch to fix this.
-
     # This request should not go through
     with pytest.raises(ValidationError, match="Invalid URL") as exec_info:
         url_validator("https://www.google.com/")
@@ -100,16 +93,10 @@ def test_hostname_blacklist_rules():
     url_validator("https://www.otherdomain.com")
 
 
-@responses.activate
 @override_settings(
     BASEROW_WEBHOOKS_URL_REGEX_BLACKLIST=[URL_BLACKLIST_ONLY_ALLOWING_GOOGLE_WEBHOOKS]
 )
 def test_hostname_blacklist_rules_only_allow_one_host():
-    responses.add(responses.GET, "https://google.com", status=200)
-    responses.add(responses.GET, "http://google.com", status=200)
-    responses.add(responses.GET, "http://1.1.1.1", status=200)
-    responses.add(responses.GET, "https://1.1.1.1", status=200)
-
     url_validator("https://www.google.com/")
     url_validator("https://google.com/")
 
@@ -122,17 +109,11 @@ def test_hostname_blacklist_rules_only_allow_one_host():
     assert exec_info.value.code == "invalid_url"
 
 
-@responses.activate
 @override_settings(
     BASEROW_WEBHOOKS_IP_BLACKLIST=[ip_network("1.0.0.0/8")],
     BASEROW_WEBHOOKS_IP_WHITELIST=[ip_network("1.1.1.1/32")],
 )
 def test_advocate_combination_of_whitelist_blacklist_rules():
-    responses.add(responses.GET, "https://1.1.1.1", status=200)
-    responses.add(responses.GET, "https://1.1.1.2", status=200)
-    responses.add(responses.GET, "http://127.0.0.1/", status=200)
-    responses.add(responses.GET, "https://2.2.2.2/", status=200)
-
     url_validator("https://1.1.1.1/")
 
     with pytest.raises(ValidationError, match="Invalid URL") as exec_info:
@@ -148,18 +129,12 @@ def test_advocate_combination_of_whitelist_blacklist_rules():
     url_validator("https://2.2.2.2/")
 
 
-@responses.activate
 @override_settings(
     BASEROW_WEBHOOKS_URL_REGEX_BLACKLIST=[URL_BLACKLIST_ONLY_ALLOWING_GOOGLE_WEBHOOKS],
     BASEROW_WEBHOOKS_IP_BLACKLIST=[ip_network("1.0.0.0/8")],
     BASEROW_WEBHOOKS_IP_WHITELIST=[ip_network("1.1.1.1/32")],
 )
 def test_advocate_hostname_blacklist_overrides_ip_lists():
-    responses.add(responses.GET, "https://1.1.1.1", status=200)
-    responses.add(responses.GET, "https://1.1.1.2", status=200)
-    responses.add(responses.GET, "http://127.0.0.1/", status=200)
-    responses.add(responses.GET, "https://2.2.2.2/", status=200)
-
     with pytest.raises(ValidationError, match="Invalid URL") as exec_info:
         url_validator("https://1.1.1.1/")
     assert exec_info.value.code == "invalid_url"
