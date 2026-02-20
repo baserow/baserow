ce56034f4f5c43bd8f5c5a576ba542cf80540f6b
Jonathan Adeline <jonathan@baserow.io>
2026-02-20 10:49:22 +0400

    filter out 404 from being sent to Sentry

--- web-frontend/sentry.server.config.ts
+++ web-frontend/sentry.server.config.ts
@@ -10,5 +10,13 @@ if (dsn && dsn !== '') {
     release: `baserow-web-frontend@${config.public.version}`,
     environment: config.public.sentryEnvironment || 'production',
     tracesSampleRate: 1.0,
+    beforeSend(event) {
+      // Don't report 404 errors
+      const statusCode =
+        event.contexts?.response?.status_code ??
+        (event.exception?.values?.[0]?.value?.includes('404') ? 404 : null)
+      if (statusCode === 404) return null
+      return event
+    },
   })
 }
