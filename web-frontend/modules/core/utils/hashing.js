356cb40ebd66388c0a4b66272d28333d91f16b4b
Przemyslaw Kukulski <przemyslaw+gh@baserow.io>
2026-02-20 09:25:51 +0100

    Ensure generateHash operate on strings

--- web-frontend/modules/core/utils/hashing.js
+++ web-frontend/modules/core/utils/hashing.js
@@ -2,5 +2,5 @@ import { sha256 } from 'js-sha256'
 
 export function generateHash(value) {
   // TODO MIG do we want to use browser async version
-  return sha256(value)
+  return sha256(String(value))
 }
