ab6c0e1684381b18c9f6cb99464cc5aa3ed16698
Przemyslaw Kukulski <przemyslaw+gh@baserow.io>
2026-03-12 11:18:11 +0100

    Review feedback v2

--- web-frontend/modules/database/fieldTypes.js
+++ web-frontend/modules/database/fieldTypes.js
@@ -111,6 +111,7 @@ import RowCardFieldEmail from '@baserow/modules/database/components/card/RowCard
 import RowCardFieldFile from '@baserow/modules/database/components/card/RowCardFieldFile'
 import RowCardFieldFormula from '@baserow/modules/database/components/card/RowCardFieldFormula'
 import RowCardFieldLinkRow from '@baserow/modules/database/components/card/RowCardFieldLinkRow'
+import GroupByFieldLinkRow from '@baserow/modules/database/components/view/grid/GroupByFieldLinkRow'
 import RowCardFieldMultipleSelect from '@baserow/modules/database/components/card/RowCardFieldMultipleSelect'
 import RowCardFieldNumber from '@baserow/modules/database/components/card/RowCardFieldNumber'
 import RowCardFieldRating from '@baserow/modules/database/components/card/RowCardFieldRating'
@@ -1343,6 +1344,10 @@ export class LinkRowFieldType extends FieldType {
 
   getCardComponent() {
     return RowCardFieldLinkRow
+  }
+
+  getGroupByComponent() {
+    return GroupByFieldLinkRow
   }
 
   getRowHistoryEntryComponent() {
@@ -4245,6 +4250,14 @@ export class FormulaFieldType extends mix(
 
   getCardComponent() {
     return RowCardFieldFormula
+  }
+
+  getGroupByComponent(field) {
+    const formulaType = this.getFormulaType(field)
+    if (formulaType) {
+      return formulaType.getCardComponent(field)
+    }
+    return this.getCardComponent(field)
   }
 
   getFilterInputComponent(field, filterType) {
