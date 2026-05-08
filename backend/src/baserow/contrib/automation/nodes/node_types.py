7b03630c7c72842486a1d45e55f243b7f8d36ebc
Tsering Paljor <tsering@baserow.io>
2026-05-08 16:46:59 +0700

    Add get_dispatch_cost() method

--- backend/src/baserow/contrib/automation/nodes/node_types.py
+++ backend/src/baserow/contrib/automation/nodes/node_types.py
@@ -71,6 +71,7 @@ from baserow.core.services.registries import service_type_registry
 
 class AutomationNodeActionNodeType(AutomationNodeType):
     is_workflow_action = True
+    dispatch_cost = 1
 
     def before_create(self, workflow, reference_node, position, output):
         if reference_node is None:
@@ -83,6 +84,7 @@ class AutomationNodeActionNodeType(AutomationNodeType):
 
 class ContainerNodeTypeMixin:
     is_container = True
+    dispatch_cost = 0
 
     def before_delete(self, node: "ContainerNodeTypeMixin"):
         if node.workflow.get_graph().get_children(node):
@@ -173,6 +175,7 @@ class CoreHttpRequestNodeType(AutomationNodeActionNodeType):
     type = "http_request"
     model_class = CoreHTTPRequestActionNode
     service_type = CoreHTTPRequestServiceType.type
+    dispatch_cost = 0
 
 
 class CoreIteratorNodeType(ContainerNodeTypeMixin, AutomationNodeActionNodeType):
@@ -203,6 +206,7 @@ class CoreRouterActionNodeType(AutomationNodeActionNodeType):
     type = "router"
     model_class = CoreRouterActionNode
     service_type = CoreRouterServiceType.type
+    dispatch_cost = 0
 
     def has_node_on_edge(self, node: CoreRouterActionNode) -> bool:
         """
