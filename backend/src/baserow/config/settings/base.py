b86df8a131ff885a17b00278c4c9630aa85636a7
Bram Wiepjes <bramw@protonmail.com>
2026-09-15 15:46:13 +0200

    bumped version

--- backend/src/baserow/config/settings/base.py
+++ backend/src/baserow/config/settings/base.py
@@ -568,7 +568,7 @@ SPECTACULAR_SETTINGS = {
         "name": "MIT",
         "url": "https://github.com/baserow/baserow/blob/develop/LICENSE",
     },
-    "VERSION": "2.3.3",
+    "VERSION": "2.3.4",
     "SERVE_INCLUDE_SCHEMA": False,
     "TAGS": [
         {"name": "Settings"},
--- docs/installation/install-with-helm.md
+++ docs/installation/install-with-helm.md
@@ -133,7 +133,7 @@ You can specify a particular Baserow version by updating your `config.yaml`:
 ```yaml
 global:
   baserow:
-    image: 2.3.3
+    image: 2.3.4
 ```
 
 Or specify the chart version directly:
