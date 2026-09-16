a83136d23a8a68ec0d0434934b37e95985eb93bf
Tsering Paljor <tsering@baserow.io>
2026-09-16 14:00:11 +0400

    Fix migrations after merge conflict/rebase

--- backend/src/baserow/contrib/automation/migrations/0037_automationworkflowhistory_cancellation_requested_by_and_more.py
+++ backend/src/baserow/contrib/automation/migrations/0037_automationworkflowhistory_cancellation_requested_by_and_more.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('automation', '0035_coregotoactionnode'),
+        ('automation', '0036_triggered_by'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
