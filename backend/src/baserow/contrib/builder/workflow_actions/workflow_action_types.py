8961557cf4186a4ce749e9ad9aebe5d5397cdd81
Jeremie Pardou <571533+jrmi@users.noreply.github.com>
2026-09-24 12:55:04 +0200

    fix(builder): hide trashed workflow data sources

--- backend/src/baserow/contrib/builder/workflow_actions/workflow_action_types.py
+++ backend/src/baserow/contrib/builder/workflow_actions/workflow_action_types.py
@@ -68,6 +68,12 @@ from baserow.core.services.types import DispatchResult
 from baserow.core.workflow_actions.models import WorkflowAction
 
 
+class ActiveDataSourceIdField(serializers.IntegerField):
+    def get_attribute(self, instance: RefreshDataSourceWorkflowAction):
+        data_source = instance.data_source
+        return data_source.id if data_source and not data_source.trashed else None
+
+
 class NotificationWorkflowActionType(BuilderWorkflowActionType):
     type = "notification"
     model_class = NotificationWorkflowAction
@@ -204,7 +210,7 @@ class RefreshDataSourceWorkflowActionType(BuilderWorkflowActionType):
     model_class = RefreshDataSourceWorkflowAction
     serializer_field_names = ["data_source_id"]
     serializer_field_overrides = {
-        "data_source_id": serializers.IntegerField(
+        "data_source_id": ActiveDataSourceIdField(
             allow_null=True,
             default=None,
             required=False,
